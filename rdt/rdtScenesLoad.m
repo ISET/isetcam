26066c14f3b4aa1e32961adc0a5096df0f2d7dc8
Brian Wandell <wandell@stanford.edu>
2018-07-06 16:33:12 -0700

    Copied more modern rdt functions from isetL3.  Will delete them from isetL3

--- rdt/rdtScenesLoad.m
+++ rdt/rdtScenesLoad.m
@@ -1,80 +1,121 @@
 function scenes = rdtScenesLoad(varargin)
-% Load multispectral multispectral scenes using Remote Data Toolbox
+% Load multispectral data from the archiva repository
 %
-%   scenes = rdtScenesLoad(varargin)  
+% Brief description:
+%   scenes = rdtScenesLoad(varargin)
 %
-% Inputs:
-%   varargin - name value pairs for the scene parameters
-%  
-%   'rdtConfigName' - repository name ('isetbio')
-%   'fov'        - Field of view      (10)
-%   'wave'       - Wavelength samples (400:10:680);
-%   'nScenes'    - First n scenes     (1)
-%   'sceneNames' - Specific scene names, these are the artifactId 
-%    
+% This routine opens a RdtClient and downloads scenes stored in the archiva
+% client
+%
+% Optional key/value pairs
+%   'rdt name'  - RDT project - default is 'isetbio'
+%   'remote directory' - Default is '/resources/scenes/multiband/scien/faces'
+%   
+%   'n scenes'  - Number of scenes or list of integers for the scene list
+%   'wave' - 400:10:680
+%   'fov'  - 10
+%
+%   'print' - Suppress printing out list of scenes in the remote directory
+%             default is true
 % Outputs:
 %   scenes - cell array of multispectral scenes
+%   files  - cell array of files (artifacts) in the remote directory
 %
 % Example:
-%   % Load from the default list
-%     scenes = rdtScenesLoad('nScenes', 1); % Loads first scene
-%     ieAddObject(scenes{1}); sceneWindow;
+%     scenes = rdtScenesLoad();
+%     scenes = rdtScenesLoad('nScenes', 1); % Loads 1 scenes
 %
-%   % Load from the default list with your wavelength
-%     wave = 420:20:500;
-%     scenes = rdtScenesLoad('nScenes',2,'wave',wave);
-%     ieAddObject(scenes{2}); sceneWindow;
+% HJ/BW, VISTA TEAM, 2015
 %
 % See also:
-%   scarletScenesLoad
-% 
-% HJ/BW, VISTA TEAM, 2015
+%    rdtOILoad(); 
 
+% TODO
+%   We could allow nScenes to be a cell array of artifact names
+%
+
+% Examples:
+%{
+  % Get a two nice Yasuma-Nayar scenes
+  scenes = rdtScenesLoad('nscenes',2, ...
+              'remote directory','/resources/scenes/multiband/yasuma', ...
+              'fov',15);
+%}
+%{
+  scenes = rdtScenesLoad('nscenes',[7 9], ...
+              'remote directory','/resources/scenes/multiband/yasuma', ...
+              'print',false);
+   ieAddObject(scenes{2}); sceneWindow;
+%}
+%{
+   % Get two nice scien, 2009 scenes
+  scenes  = rdtScenesLoad('nscenes',2, ...
+              'remote directory','/resources/scenes/multiband/scien/2009', ...
+              'fov',15, ...
+              'wave',410:690);
+%}
+%{
+   % Get two nice scien, 2009 scenes
+  scenes = rdtScenesLoad('nscenes',[2 5], ...
+              'remote directory','/resources/scenes/multiband/scien/2009', ...
+              'fov',15, ...
+              'wave',410:690);
+   ieAddObject(scenes{1}); sceneWindow;
+   ieAddObject(scenes{2}); sceneWindow;
+%}
 %% Parse input parameters
 p = inputParser;
-addParameter(p,'sceneNames',...
-    {'JapaneseDoll','RedRose', 'AsianFemale_2','MacbethSun'},@iscell)
-addParameter(p,'nScenes', inf,@isnumeric);
-% addParameter(p,'wave', 400:10:680,@isvector);
-addParameter(p,'rdtConfigName', 'isetbio',@ischar);
-addParameter(p,'fov', 10);
-parse(p,varargin{:});
+varargin = ieParamFormat(varargin);
 
-% Retrieve the results
-nScenes    = p.Results.nScenes;
-sceneNames = p.Results.sceneNames(1:nScenes);
-% wave       = p.Results.wave;
-rdtName    = p.Results.rdtConfigName;
-fov        = p.Results.fov;
+p.addParameter('nscenes', inf);
+p.addParameter('wave', 400:10:680);
+p.addParameter('rdtname', 'isetbio');
+p.addParameter('remotedirectory','/resources/scenes/multiband/scien/faces',@ischar);
+p.addParameter('fov', 10);
+p.addParameter('print',false,@islogical);
+
+p.parse(varargin{:});
+
+nScenes = p.Results.nscenes;
+wave    = p.Results.wave;
+rdtName = p.Results.rdtname;
+fov     = p.Results.fov;
+print   = p.Results.print;
+remotedirectory = p.Results.remotedirectory;
 
 %% Init remote data toolbox client
-rdt = RdtClient(rdtName);  % using rdt-config-isetbio.json configuration file
-% rdt.crp('/resources/scenes');
+rdt = RdtClient(rdtName);  % using rdt-config-scien.json configuration file
 
-% files = rdt.listArtifacts();  
-% nScenes = min(nScenes, length(files)); % fprintf('Found %d files \n',length(files));
-% for ii=1:length(files)
-%     fprintf('%d %s\n',ii,files(ii).artifactId);
-% end
+% Tell the user what scenes.  We should create more options.
+fprintf('Loading scenes from  %s:%s\n', rdtName, remotedirectory);
+rdt.crp(remotedirectory);
+files = rdt.listArtifacts('print',print);
 
-% Some reason, we need to do this ...
-rdt.crp('');
-scenes = cell(length(sceneNames), 1);
-for ii=1:length(sceneNames)
-    artifacts = rdt.searchArtifacts(sceneNames{ii},'type','mat');
-    if isempty(artifacts)
-        warning('No artifacts named %s\n',sceneNames{ii});
-        break;
-    else
-        nArtifacts = numel(artifacts);
-        fprintf('%d artifacts match the term %s\n', nArtifacts,sceneNames{ii});
+%% Parse the nScenes variable
+if isscalar(nScenes)
+nScenes = min(nScenes, length(files));
+sList = 1:nScenes;
+elseif isvector(nScenes)
+    if min(nScenes) > 0 && max(nScenes) <= length(files)
+        sList = nScenes;
+        nScenes = length(sList);
     end
-    
-    % Create the scene cell array
-    data = rdt.readArtifacts(artifacts(1));
-    scene = sceneFromBasis(data{1});
-    % scene = sceneSet(scene,'wave',wave);
-    scenes{ii} = sceneSet(scene, 'h fov', fov);
+end
+
+%% load scene files
+scenes = cell(nScenes, 1);
+cnt = 1;
+for ii = sList
+    data = rdt.readArtifact(files(ii).artifactId);
+    % The scene data are stored in various formats.  We decode here
+    if isfield(data,'scene')
+        scene = data.scene;
+    elseif isfield(data,'basis')
+        scene = sceneFromBasis(data);
+    end
+    scene = sceneSet(scene, 'wave', wave); % adjust wavelength
+    scenes{cnt} = sceneSet(scene, 'h fov', fov);
+    cnt = cnt+1;
 end
 
 end
