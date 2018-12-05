9100076429c653d40572377b602ca0aea7a32409
Brian Wandell <wandell@stanford.edu>
2018-12-04 17:03:56 -0800

    Starting a new tutorial for ZL and students

---
+++ tutorials/sensor/t_sensorColorFilters.m
@@ -0,0 +1,13 @@
+%% Create, save, load and attach color filters to a sensor
+%
+% Students often want to experiment with cameras with different color
+% filters.  This is how to create some filters, read them in, and
+% attach them to a sensor.
+%
+% BW, ISET Team, 2018
+
+%%
+
+%%
+
+
