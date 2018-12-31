c63adf70b0c63c8e4c10eaab2cfce7801208ab47
Brian Wandell <wandell@stanford.edu>
2018-12-31 14:05:47 -0800

    Added ir and uv blocking filter options

--- sensor/colorfilter/sensorColorFilter.m
+++ sensor/colorfilter/sensorColorFilter.m
@@ -1,51 +1,121 @@
-function [fData, wave] = sensorColorFilter(cfType,varargin)
+function [fData, wave] = sensorColorFilter(cfType,wave, varargin)
 % Create color filters for use in a sensor
 %
-%     [fData, wave] = sensorColorFilter(cfType,varargin)
+% Syntax:
+%     [fData, wave] = sensorColorFilter(cfType, wave, varargin)
 %
-% Gateway routine for creating sensor color filter curves.
+% Description:
+%  Gateway routine for creating sensor color filter curves.
 %
-%  Gaussian:(Others to come)
-%
+% Required Inputs:
+%  cfType:  One of:  Gaussian, irFilter, uvFilter
+%  wave:    Wavelength samples
 %  
-% Examples:
-% Gaussian type:
-%  cfType = 'gaussian'; 
-%  [fData,wave] = sensorColorFilter(cfType);
-%  plot(wave,fData)
+% Optional inputs
+%  if Gaussian:   wave, cPos,  widths
+%  if uv filter:  wave, uvCut, smooth
+%  if ir filter:  wave, irCut, smooth
 %
-%  wave = [400:10:700]; cPos = [500,600]; width = [40,40];
-%  fData = sensorColorFilter(cfType,wave, cPos, width);
-%  plot(wave,fData);
+% Outputs:
+%   fData - Matrix of filter data in columns.  nWave x nFilters
 %
-%  cfType = 'gaussian'; wave = [350:850];
-%  cPos = 450:50:750; width = ones(size(cPos))*25;
-%  fData = sensorColorFilter(cfType,wave, cPos, width);
-%  plot(wave,fData)
+% Copyright ImagEval Consultants, LLC, 2010
 %
 % See also: 
 %  
-% Copyright ImagEval Consultants, LLC, 2010
+
+%  
+% Examples:
+%{
+% Gaussian type:
+ cfType = 'gaussian'; 
+ [fData,wave] = sensorColorFilter(cfType);
+ plot(wave,fData)
+%}
+%{
+ wave = [400:10:700]; cPos = [500,600]; width = [40,40];
+ fData = sensorColorFilter(cfType,wave, cPos, width);
+ plot(wave,fData);
+%}
+%{
+  cfType = 'gaussian'; wave = [350:850];
+  cPos = 450:50:750; width = ones(size(cPos))*25;
+  fData = sensorColorFilter(cfType,wave, cPos, width);
+  plot(wave,fData)
+%}
+%{
+  cfType = 'ir filter'; wave = 400:800; irCut = 680; smooth = 5;
+  f = sensorColorFilter(cfType,wave,irCut,smooth);
+  vcNewGraphWin; plot(wave,f);
+%}
+%{
+  cfType = 'uv Filter'; wave = 350:800; uvCut = 440; smooth = 7;
+  f = sensorColorFilter(cfType,wave,uvCut,smooth);
+  vcNewGraphWin; plot(wave,f);
+%}
 
 if ieNotDefined('cfType'), cfType = 'gaussian'; end
+if notDefined('wave'),     wave = 400:700; end
+
+smooth = -1;
+
+cfType = ieParamFormat(cfType);
 
 switch lower(cfType)
     case 'gaussian'  
-        if isempty(varargin), wave = 400:700; 
-        else wave = varargin{1}; end
-        if length(varargin)<2, cPos = [450,550,650]; 
-        else cPos = varargin{2}; end
-        if length(varargin)<3, widths = ones(size(cPos))*40; 
-        else widths = varargin{3}; end
+        % A set of gaussian filters centered at cPos (vector) and widths
+        % (vector)
+        
+        if length(varargin)<1, cPos = [450,550,650];
+        else, cPos = varargin{1}; end
+        
+        if length(varargin)<2, widths = ones(size(cPos))*40; 
+        else, widths = varargin{2}; end
 
         nFilters = length(cPos);
         fData = zeros(length(wave),nFilters);
         for ii=1:nFilters
             fData(:,ii) = exp(-1/2*( (wave-cPos(ii))/(widths(ii))).^2);
         end
-        % plot(wave,fData)
+
+    case 'irfilter'
+        % Infrared blocking filter.
+        if length(varargin) < 1, cPos = 700; 
+        else, cPos = varargin{1}; 
+        end
+        if length(varargin) > 1, smooth = varargin{2}; end
+
+        fData = ones(size(wave));
+        lst = (wave > cPos);
+        fData(lst) = 0;
+
+    case 'uvfilter'
+        % Ultraviolet blocking filter.
+        if length(varargin) < 1, cPos = 400; 
+        else, cPos = varargin{1}; 
+        end
+        if length(varargin) > 1, smooth = varargin{2}; end
+        
+        fData = ones(size(wave));
+        lst = (wave < cPos);
+        fData(lst) = 0;
+        
     otherwise
-        error('Unknown filter type')
+        error('Unknown filter type %s\n',cfType)
 end
 
-return
+if smooth > 0
+    g = fspecial('gaussian',[5*smooth,1],smooth);
+    fData = conv(fData,g,'same');
+    
+    switch (cfType)
+        case 'gaussian'
+            % No more smoothing.  User should change width
+            disp('No gaussian smoothing');
+        case 'uvfilter'
+            fData((end-(2*smooth)):end) = 1;
+        case 'irfilter'
+            fData(1:(2*smooth)) = 1;
+    end
+    
+end
