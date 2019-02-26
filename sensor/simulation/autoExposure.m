6e5d307d67bc2fb7fde03a24e89bf61448b102c6
Brian Wandell <wandell@stanford.edu>
2019-02-26 10:57:40 -0800

    Comments and starting

--- sensor/simulation/autoExposure.m
+++ sensor/simulation/autoExposure.m
@@ -1,30 +1,60 @@
-function  [integrationTime,maxSignalVoltage,smallOI] = autoExposure(OI,sensor,level,aeMethod)
+function  [integrationTime,maxSignalVoltage,smallOI] = autoExposure(oi,sensor,level,aeMethod,varargin)
 %Gateway routine to auto-exposure methods
 %
-%     [integrationTime,maxSignalVoltage,smallOI] = autoExposure(OI,sensor,[level = 0.95],[aeMethod])
+%     [integrationTime,maxSignalVoltage,smallOI] = autoExposure(oi,sensor,[level = 0.95],[aeMethod='default'],varargin)
 %
 %  Find an integration time (sec) that produces a voltage level at a
 %  fraction (0 < level < 1) of the voltage swing.  The data used to set the
 %  level are from the signal current image plus the dark current image.
 %
+% Inputs:
+%  OI:       The optical image
+%  sensor:   The sensor
+%  level:    Fraction of the voltage swing (default is 0.95)
+%  aeMethod: Which method, default is 'luminance'.  See full set of
+%            options below.
+%  
+% Optional key/value pairs
+%  centerrect:  Central rectangle for 'center' method
+%
+% Outputs:
+%   integrationTime:  In seconds
+%   maxSignalVotage:
+%   smallOI:
+%
+%
 %  The currently implemented methods are
 %
-%     'default'     - Integration time based on point with highest luminance
-%     'luminance'   - Extracts the brightest part of the image and returns the
-%                     integration time so that the brightest part is at
-%                     leve of the voltage swing. 
-%     'aeMean'      - Achieve a mean voltage level as fraction of voltage
-%                     swing
-%     'aeFull'      - Confusing.
-%     'aeCFA'       - Compute separately for each color filter type
-%     'aeSpecular'  - A certain percentage can be saturated
+%     'default'     - The default method finds the signal voltage
+%                     for a one sec exposure of the portion of the
+%                     optical image with peak illuminance.  The
+%                     exposure routine then returns an integration
+%                     time that produces a value of "level" times the
+%                     voltage swing. (aeLuminance)
+%     'luminance'   - Same as default (aeLuminance)
+%     'full'        - Finds the maximum signal voltage for a one sec
+%                     exposure, and then returns an integration time
+%                     that produces a value of 'level' times the
+%                     voltage swing (aeFull)
+%     'specular'    - Make the mean voltage level as fraction of
+%                     voltage swing. (aeSpecular)
+%     'cfa'         - Compute separately for each color filter type
+%     'mean'        - Finds the sensor intergration time to achieve
+%                     the desired mean voltage level expressed as a
+%                     fraction of the voltage swing (aeMean). For example
+%                     setting level of 0.3 means that
 %
-% The default method finds the signal voltage for a one sec exposure of the
-% portion of the optical image with peak illuminance.  The exposure routine
-% then returns an integration time that produces a value of "level" times
-% the voltage swing.  
+%                     mean(signalVoltage(:)) = 0.3*voltageSwing
+%
+%      'center'     - Set the exposure duration as in 'luminance', but
+%                     using a rect from the center of the image
 %
 % Copyright ImagEval Consultants, LLC, 2003.
+
+% Examples:
+%{
+
+%}
 
 % TODO: This routine needs to be expanded to include the other methods.
 % Good opportunities for matrix-metering with weighted matrices, and
@@ -32,18 +62,32 @@ function  [integrationTime,maxSignalVoltage,smallOI] = autoExposure(OI,sensor,le
 
 if ieNotDefined('level'), level = 0.95; end
 if ieNotDefined('aeMethod'), aeMethod = 'default'; end
+p = inputParser;
+
+p.addRequired('oi');
+p.addRequired('sensor');
+p.addRequired('level',@isscalar);
+p.addRequired('aeMethod',@ischar)
+
+defaultRect = '';
+
+p.addParameter('centerrect',defaultRect,@isvector);
+p.parse(oi,sensor,level,aeMethod,varargin{:});
+centerRect = p.Results.centerrect;
 
 switch lower(aeMethod)
-    case 'default'
-        [integrationTime,maxSignalVoltage,smallOI] = aeLuminance(OI,sensor,level);
-    case 'luminance'
-        [integrationTime,maxSignalVoltage] = aeFull(OI,sensor,level);
+    case {'default','luminance'}
+        [integrationTime,maxSignalVoltage,smallOI] = aeLuminance(oi,sensor,level);
+    case 'full'
+        [integrationTime,maxSignalVoltage] = aeFull(oi,sensor,level);
     case 'cfa'
-        [integrationTime,maxSignalVoltage] = aeCFA(OI,sensor,level);  
+        [integrationTime,maxSignalVoltage] = aeCFA(oi,sensor,level);  
     case 'specular'
-        [integrationTime,maxSignalVoltage] = aeSpecular(OI,sensor,level); 
+        [integrationTime,maxSignalVoltage] = aeSpecular(oi,sensor,level); 
     case 'mean'
-        [integrationTime,maxSignalVoltage] = aeMean(OI,sensor,level); 
+        [integrationTime,maxSignalVoltage] = aeMean(oi,sensor,level); 
+    case 'weighted'
+        [integrationTime,maxSignalVoltage] = aeWeighted(oi,sensor,level);
     otherwise
         error('Unknown auto-exposure method')
 end
@@ -202,3 +246,10 @@ maxSignalVoltage = max(signalVoltage(:));
 
 end
 
+%-------------------------------
+function [integrationTime, maxSignalVoltage] = aeWeighted(oi,sensor,level,rect)
+voltageSwing = sensorGet(sensor,'pixel voltage swing');
+
+centerOI = oiCrop(OI,rect);
+
+end
