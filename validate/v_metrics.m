713a38e782e4494ed2adc2c267a21926075bedfb
Brian Wandell <wandell@stanford.edu>
2019-04-22 11:44:08 -0700

    Tested all validation scripts

--- validate/v_metrics.m
+++ validate/v_metrics.m
@@ -2,9 +2,9 @@
 %
 % Scripts related to metrics
 
+% Complains that this is broken but I don't see why.  They each run.
 s_metricsMTFSlantedBarInfrared
 v_scielabExample
 s_scielabPatches
-% v_metricsCamera
 
 %%
