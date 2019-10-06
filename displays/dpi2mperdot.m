2f6a20a9425e12df51e10b68c9f1f96278d08b0e
Brian Wandell <wandell@stanford.edu>
2019-10-05 20:26:33 -0700

    Bug in mperdot2dpi.  Reported by Kaijun Feng

--- displays/dpi2mperdot.m
+++ displays/dpi2mperdot.m
@@ -1,22 +1,31 @@
 function mpd = dpi2mperdot(dpi,unit)
-% Convert dots per inch to meters format (microns)
+% Convert dots per inch to meters format (microns by default)
 %
+% Brief syntax
 %    mpd = dpi2mperdot(dpi,[unit])
 %
-% Both dpi and microns are commonly used to specify display pixel size.
-% We make it easy to convert from dots per inch to microns per dot.
+% Brief descriptions
+%   Both dots per inch (dpi) and microns are commonly used to specify
+%   display pixel size. We make it easy to convert from dpi to
+%   microns per dot. 
 %
-% Default output is microns per dot.  For meters or cm use the optional
-% unit argument (e.g., 'cm', 'm').
+% Inputs
+%   dpi:  Dots per inch
+%
+% Inputs (optional)
+%   unit: Default output is microns per dot.  For meters or cm use the
+%         optional unit argument (e.g., 'cm', 'm').
 %
 % Example:
 %   dpi = 120;
-%   mpd = dpi2mperdot(dpi)
-%   mpd = dpi2mperdot(dpi,'um')
-%   mpd = dpi2mperdot(dpi,'meters')
-%   cmpd= dpi2mperdot(dpi,'cm')
+%   mpd = dpi2mperdot(dpi); fprintf('%f microns per dot\n',mpd);
+%   mpd = dpi2mperdot(dpi,'um'); fprintf('%f microns per dot\n',mpd);
+%   mpd = dpi2mperdot(dpi,'meters'); fprintf('%f meters per dot\n',mpd);
 %
-% See also: mperdot2dpi
+%   dpi = mperdot2dpi(mpd)    % Back again
+%
+% See also: 
+%    mperdot2dpi
 
 if ieNotDefined('unit'), unit = 'um'; end
 
--- displays/mperdot2dpi.m
+++ displays/mperdot2dpi.m
@@ -1,31 +1,29 @@
-function dpi = mperdot2dpi(mpd,unit)
-% Convert dots per inch to size in microns per dot
+function dpi = mperdot2dpi(mpd)
+% Convert microns per dot to dots per inch
 %
-%    dpi = mperdot2dpi(mpd,[unit])
+% Syntax:
+%    dpi = mperdot2dpi(mpd)
 %
-% Both dpi and microns are commonly used to specify display pixel size.
-% We make it easy to convert from dots per inch to meters per dot.  
+% Brief description:
+%   Both dots per inch (dpi) and microns per dot (mpd) are commonly used to
+%   specify display pixel size. We make it easy to convert from dots per
+%   inch to meters per dot.
 %
-% By specifying the unit, you can also have values returned as mm per dot or
-% microns per dot.  The units are 'mm','um','m','cm'
+% Input:
+%   mpd:   Microns per dot
 %
 % Example:
-%   dpi = mperdot2dpi(254,'mm')
-%   dpi = mperdot2dpi(300,'um')
-%   dpi = mperdot2dpi(.300,'cm')
-%   dpi = mperdot2dpi(.300,'mm')
-%   dpi = mperdot2dpi(.300,'um')
+%   mpd = dpi2mperdot(100);   % Dots per inch to microns per dot
+%   dpi = mperdot2dpi(mpd)    % Back again
 %
-% See also dpi2mperdot
+% See also
+%  dpi2mperdot
 %
-
-if ieNotDefined('unit'), unit = 'meters'; end
 
 % 1/mpd is dots per micron
 % 2.54*1e4 is microns per inch
-% 2.54*1e-2 ~ (1/39.37007874015748) is meters per inch
-dpi = (1 /mpd) * (1/39.37007874015748); 
+dpi = (1 /mpd) * (2.54*1e4);   % dots per micron * meters per inch
 
-dpi = ieUnitScaleFactor(unit)*dpi;
+% dpi = (1 /mpd) * (1/39.37007874015748);   % dots per micron * meters per inch
 
-return
+end
