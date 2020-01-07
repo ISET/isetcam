bacb5c99fa117ad29dad7d94c017b5a0ac84faa4
Brian Wandell <wandell@stanford.edu>
2020-01-06 22:37:57 -0800

    Added switch

--- utility/plots/plotRadiance.m
+++ utility/plots/plotRadiance.m
@@ -1,15 +1,15 @@
-function hdl = plotRadiance(wavelength,radiance)
+function hdl = plotRadiance(wavelength,radiance,varargin)
 % Utility to plot and label radiance data
 %
 % Syntax
-%   hdl = plotRadiance(wavelength,radiance)
+%   hdl = plotRadiance(wavelength,radiance,varargin)
 %
 % Inputs
 %    wavelength  - Wavelength samples (nm)
 %    radiance    - Columns of radiances
 %
 % Optional key/value pairs
-%    None
+%    title
 %
 % Returns;
 %   hdl
@@ -21,9 +21,23 @@ function hdl = plotRadiance(wavelength,radiance)
 % More options to come.  Such as a title.
 % 
 
+%%
+p = inputParser;
+p.addRequired('wavelength',@isvector);
+p.addRequired('radiance',@isnumeric);
+
+p.addParameter('title','Spectral radiance',@ischar);
+
+p.parse(wavelength,radiance,varargin{:});
+strTitle = p.Results.title;
+
+%% Open the window
 hdl = ieNewGraphWin;
 wavelength = wavelength(:);
 
+%% Handle the case of a transpose in radiance
+
+% The dimension that matches wavelength is the right one
 nWave = length(wavelength);
 if nWave == size(radiance,1)
     plot(wavelength(:),radiance);
@@ -31,9 +45,11 @@ elseif length(wavelength) == size(radiance,2)
     plot(wavelength(:),radiance');    
 end
 
+%% Label it
+
 xlabel('Wavelength (nm)');
 ylabel('Radiance (watts/sr/nm/m^2)');
 grid on; 
-title('Spectral radiance');
+title(strTitle);
 
 end
