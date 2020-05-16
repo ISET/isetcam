70abb5bc49e35c9b9c972ad3601e56ef6f89e9e6
Brian Wandell <wandell@stanford.edu>
2020-05-16 15:27:19 -0700

    Debugging

--- validate/v_oiIlluminant.m
+++ validate/v_oiIlluminant.m
@@ -3,7 +3,10 @@
 %  We sometimes include illuminant in the OI when we render a 3D scene with
 %  PBRT (ISET3D).  Typically these are spatial-spectral illuminants
 %
-
+% See also
+%   oiIlluminantSS, oiIlluminantPattern, sceneIlluminantSS,
+%   sceneIlluminantPattern
+%
 
 %%
 ieInit;
@@ -15,11 +18,11 @@ oi = oiSet(oi,'illuminant',thisI);
 testI = oiGet(oi,'illuminant');
 assert(isequal(thisI,testI));
 
-%%
+%%  This reads the data in the illuminant and reports the format
+
 oiGet(oi,'illuminant format')
 
-oiSet(oi,'illuminant format','spatial spectral')
 
-
+%% END
 
 
