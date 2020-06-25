41dc2fdd3f67bfa8c23b1d8d116f70921e5f56eb
Brian Wandell <wandell@stanford.edu>
2020-06-24 21:41:20 -0700

    Comments and updates

--- utility/ieManualViewer.m
+++ utility/ieManualViewer.m
@@ -12,46 +12,54 @@ function ieManualViewer(mType)
 %
 % Copyright ImagEval Consultants, LLC, 2006.
 
-% TODO:  Should we have a more graceful response if the user is offline?
+%{
+ ieManualViewer;
+%}
+%{
+ieManualViewer('github');
+%}
+%{
+ieManualViewer('iset functions');
+%}
+%{
+ieManualViewer('scene functions');
+%}
 
-% Defaults
+%%  Defaults
 if ieNotDefined('mType')
+    % Take them to the ISETCam wiki
     web('https://github.com/iset/isetcam/wiki','-browser');
     return;
-else
-    mType = ieParamFormat(mType);
-    switch mType
-        case {'isetfunctions'}
-            % m2html overview for old-timers
-            web('https://scarlet.stanford.edu/~brian/isetcam/manuals/index.html','-browser');
-        case {'github'}
-            % Main
-            web('https://github.com/ISET/isetcam','-browser')
-            % Separate directories
-        case {'scenefunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/scene','-browser');
-        case {'oifunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/opticalimage','-browser');
-        case {'opticsfunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/opticalimage/optics','-browser');
-        case {'sensorfunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/sensor','-browser');
-        case {'pixelfunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/sensor/pixel','-browser');
-        case {'ipfunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/imgproc','-browser');
-        case {'metricsfunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/metrics','-browser');
-        case {'displayfunctions'}
-            web('https://github.com/ISET/isetcam/tree/master/displays','-browser');
-        otherwise
-            web('https://github.com/iset/isetcam/wiki','-browser');
-    end
 end
 
+%% Options
+
+mType = ieParamFormat(mType);
+switch mType
+    case {'isetfunctions'}
+        % m2html overview for old-timers
+        web('https://scarlet.stanford.edu/~brian/isetcam/manuals/index.html','-browser');
+    case {'github'}
+        % Main
+        web('https://github.com/ISET/isetcam','-browser')
+        % Separate directories
+    case {'scenefunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/scene','-browser');
+    case {'oifunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/opticalimage','-browser');
+    case {'opticsfunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/opticalimage/optics','-browser');
+    case {'sensorfunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/sensor','-browser');
+    case {'pixelfunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/sensor/pixel','-browser');
+    case {'ipfunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/imgproc','-browser');
+    case {'metricsfunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/metrics','-browser');
+    case {'displayfunctions'}
+        web('https://github.com/ISET/isetcam/tree/master/displays','-browser');
+    otherwise
+        web('https://github.com/iset/isetcam/wiki','-browser');
 end
-
-
-
-
-
+end
