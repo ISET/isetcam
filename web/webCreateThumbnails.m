12aa53bcdbde7b5bd142333abe1916ec64119311
David Cardinal <github@proshooters.com>
2020-08-21 08:52:16 -0700

    added option for HDR+multispectral decoding

--- web/webCreateThumbnails.m
+++ web/webCreateThumbnails.m
@@ -6,6 +6,14 @@ ourFiles = dir(fullfile(folderPath,"*.mat"));
 for i = 1:length(ourFiles)
     fullPath = fullfile(ourFiles(i).folder, ourFiles(i).name);
     ourScene = sceneFromFile(fullPath, 'multispectral');
-    thumbName = strrep(fullPath, ".mat", ".png");
-    sceneSaveImage(ourScene, thumbName);
+    useHDR = true; % for HDR rendering
+    if useHDR
+        rgbImage = sceneShowImage(ourScene, 3); % try hdr rendering
+        thumbName = strrep(fullPath, ".mat", ".png");
+        imwrite(rgbImage, thumbName);
+    else
+        thumbName = strrep(fullPath, ".mat", ".png");
+        sceneSaveImage(ourScene, thumbName);
+    end
+end
 end
