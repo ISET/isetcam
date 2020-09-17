97c8964535ef97474c2b87c786e0266bfd26abcf
David Cardinal <github@cardinalphoto.com>
2020-09-16 20:08:19 -0700

    Initial commit of save/restore window positions

--- gui/windows/ieWindowsSet.m
+++ gui/windows/ieWindowsSet.m
@@ -1,4 +1,4 @@
-function wPos = ieWindowsSet(wPos)
+function wPos = ieWindowsSet(wPos, wState)
 % Set current window positions and sizes
 %
 %  wPos = ieWindowsSet([wPos])
@@ -29,7 +29,17 @@ function wPos = ieWindowsSet(wPos)
 %% The window positions are stored in the Matlab pref structure
 
 % By default, we get the preferred positions and put any open windows there
-if ieNotDefined('wPos'), wPos = getpref('ISET','wPos'); end
+if ieNotDefined('wPos') || isempty(wPos), wPos = getpref('ISET','wPos'); end
+
+if ~exist('wState', 'var') || isempty(wState)
+    wState = getpref('ISET','wState', []);
+    restoreState = false;
+else
+    restoreState = true;
+    if wState == true
+        wState = getpref('ISET','wState', []);
+    end
+end
 
 % If a file name is passed, then we read wPos from that file and put the
 % windows there.
@@ -45,31 +55,68 @@ end
 %% If the window is created and there is a stored value, set the window
 
 w = ieSessionGet('main window'); v = wPos{1};
-if ~isempty(w) && ~isempty(v) && isvalid(w), w.figure1.Position = v; end
+if ~isempty(w) && ~isempty(v) && isvalid(w)
+    w.figure1.Position = v; 
+    if restoreState && ~isempty(wState) && numel(wState) >= 1
+        w.figure1.WindowState = wState{1};
+    end
+end
 
 w = ieSessionGet('scene window'); v = wPos{2};
-if ~isempty(w) && ~isempty(v) && isvalid(w), w.figure1.Position = v; end
+if ~isempty(w) && ~isempty(v) && isvalid(w)
+    w.figure1.Position = v;
+    if restoreState && ~isempty(wState) && numel(wState) >= 2
+        w.figure1.WindowState = wState{2};
+    end
+end
 
 w = ieSessionGet('oi window'); v = wPos{3};
-if ~isempty(w) && ~isempty(v) && isvalid(w), w.figure1.Position = v; end
+if ~isempty(w) && ~isempty(v) && isvalid(w)
+    w.figure1.Position = v;
+    if restoreState && ~isempty(wState) && numel(wState) >= 3
+        w.figure1.WindowState = wState{3};
+    end
+end
 
 w = ieSessionGet('sensor window');v = wPos{4};
-if ~isempty(w) && ~isempty(v) && isvalid(w), w.figure1.Position = v; end
+if ~isempty(w) && ~isempty(v) && isvalid(w)
+    w.figure1.Position = v;
+    if restoreState && ~isempty(wState) && numel(wState) >= 4
+        w.figure1.WindowState = wState{4};
+    end
+end
 
 w = ieSessionGet('ip window');v = wPos{5};
-if ~isempty(w) && ~isempty(v) && isvalid(w), w.figure1.Position = v; end
+if ~isempty(w) && ~isempty(v) && isvalid(w)
+    w.figure1.Position = v;
+    if restoreState && ~isempty(wState) && numel(wState) >= 5
+        w.figure1.WindowState = wState{5};
+    end
+end
 
 if numel(wPos) > 5
      w = ieSessionGet('camdesign window');v = wPos{6};
-     if ~isempty(w) && ~isempty(v) && isvalid(w), w.figure1.Position = v; end
+     if ~isempty(w) && ~isempty(v) && isvalid(w)
+         w.figure1.Position = v;
+        if restoreState && ~isempty(wState) && numel(wState) >= 6
+            w.figure1.WindowState = wState{6};
+        end
+     end
 end
 
 if numel(wPos) > 6
      w = ieSessionGet('imageexplore window');v = wPos{7};
-     if ~isempty(w) && ~isempty(v) && isvalid(w), w.UIFigure.Position = v; end
+     if ~isempty(w) && ~isempty(v) && isvalid(w)
+         w.UIFigure.Position = v;
+         if restoreState && ~isempty(wState) && numel(wState) >= 7
+            w.UIFigure.WindowState = wState{7};
+         end
+     end
 end
 
 setpref('ISET','wPos',wPos);
+% not sure if/when we need to store State here since we use
+% WindowsGet(true)
 
 end
 
