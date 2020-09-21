8b6c8675eb32b8bb3610f775c4b1191f540f1ce2
Brian Wandell <wandell@stanford.edu>
2020-09-20 21:37:02 -0700

    Inserted some tests

--- gui/vcNewGraphWin.m
+++ gui/vcNewGraphWin.m
@@ -1,7 +1,9 @@
 function figHdl = vcNewGraphWin(figHdl, fType, varargin)
-% Deprecated:  Calls ieNewGraphWin;
+% Deprecated:  Now it simply calls ieNewGraphWin;
 %
-
+% vcNewGraphWin;
+% vcNewGraphWin([],'wide');
+% vcNewGraphWin([],'tall');
 if ~exist('figHdl','var'), figHdl = []; end
 if ~exist('fType','var'), fType = 'default'; end
 
@@ -9,51 +11,4 @@ figHdl = ieNewGraphWin(figHdl, fType, varargin{:});
 
 end
 
-%{
-if ieNotDefined('figHdl'), figHdl = figure; end
-if ieNotDefined('fType')  
-    fType = 'default'; 
-    wPos = ieSessionGet('wpos');
-    wPos = wPos{6};
-    if isempty(wPos), fType = 'upperleft'; end
-end
-
-set(figHdl,'Name','ISET GraphWin','NumberTitle','off');
-set(figHdl,'CloseRequestFcn','ieCloseRequestFcn');
-set(figHdl,'Color',[1 1 1]);
-
-% Position the figure
-fType = ieParamFormat(fType);
-switch(fType)
-    case 'default'
-        % Use the getpref window position for the graph window
-        set(figHdl,'Units','normalized','Position',wPos);
-    case 'upperleft'
-        set(figHdl,'Units','normalized','Position',[0.007 0.55  0.28 0.36]);
-    case 'tall'
-        set(figHdl,'Units','normalized','Position',[0.007 0.055 0.28 0.85]);
-    case 'wide'
-        set(figHdl,'Units','normalized','Position',[0.007 0.62  0.60  0.3]);
-    case {'upperleftbig','big'}
-        % Like upperleft but bigger
-        set(figHdl,'Units','normalized','Position',[0.007 0.40  0.40 0.50]);
-    otherwise % Matlab default
-end
-
-%% Apply the varargin arguments as key/value pairs
-if ~isempty(varargin)
-    n = length(varargin);
-    if ~mod(n,2)
-        for ii=1:2:(n-1)
-            set(figHdl,varargin{ii},varargin{ii+1});
-        end
-    end
-end
-
-%% Store some information.  Not sure it is needed; not much used.
-ieSessionSet('graphwinfigure',figHdl);
-ieSessionSet('graphwinhandle',guidata(figHdl));
-
-end
-%}
 
