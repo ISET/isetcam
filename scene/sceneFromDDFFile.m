055421d1d7725518c15a95d06958c729129a0c84
digital-pro <github@cardinalphoto.com>
2020-10-11 17:08:46 -0700

    can get depth maps now

---
+++ scene/sceneFromDDFFile.m
@@ -0,0 +1,22 @@
+function [scene,fName] = sceneFromDDFFile(fName, imType, meanLuminance, dispCal, ...
+    wList, varargin)
+%SCENEFROMDDFFILE Summary of this function goes here
+%   Detailed explanation goes here
+
+% First we call the basic scene creation routine
+% it should probably take depth as a parameter, but it seems to have 
+% a lot of other legacy parameter stuff, so doing it here for now
+scene = sceneFromFile(fName, imType, meanLuminance, dispCal, ...
+    wList, varargin);
+
+% right now we only support depth that is compatible with Google's approach
+depthMap = exiftoolDepthFromFile(fName, 'type', 'GooglePixel');
+
+% NEED TO DECIDE IF DEPTHMAP IS ALREADY METERS HERE, OR IS A JPEG???
+%depthImage = imload(depthmap);
+
+scene = sceneSet(scene, 'depth map', depthMap);
+
+
+end
+
