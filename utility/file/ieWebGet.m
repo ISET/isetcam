d332e45d12385adcb928cd4ff790351972452f48
Brian Wandell <wandell@stanford.edu>
2020-10-14 12:06:29 -0700

    Moving ieWebGet along a little bit more.  Nothing tested by BW yet.

--- utility/file/ieWebGet.m
+++ utility/file/ieWebGet.m
@@ -1,8 +1,12 @@
+function ieWebGet(varargin)
 %% Download a file from the Stanford web site
 %
+%
+% See also
+%
 
-function ieWebGet(varargin)
 
+% Examples
 %{
 filetype - hyperspectral, multispectral, hdr, pbrt, ....
 readonly - (possible for images)
@@ -22,14 +26,26 @@ readonly - (possible for images)
   dataFromFile   = ieWebGet(listOfTheFiles{ii},'type','V3','readonly',true);
 %}
 
+%% Decode key/val args
+
+varargin = ieParamFormat(varargin);
+
 p = inputParser;
-p.addRequired('name');
-p.addParameter('scenetype');
+p.addRequired('remotename');                % Remote file name
+p.addParameter('localname','',@ischar);     % Defaults to remote name
+p.addParameter('scenetype','pbrt',@ischar); % Helps find the file
+p.addParameter('verbose',true,@islogical);  % Tell the user what happened
+p.addParameter('list',false,@islogical);    % Assume file download, not dir list
 p.parse(varargin);
 
-sceneName   = p.Results.name;
+remoteName  = p.Results.name;
 sceneType   = p.Results.scenetype;
+verbose     = p.Results.verbose;
+localName   = p.Results.localname;
+if isempty(localName), localName = remoteName; end
 
+
+%%
 switch sceneType
     case 'pbrt'
         if exist(piRootPath, 'var') && isfolder(piRootPath) 
@@ -41,18 +57,32 @@ switch sceneType
         end
         % for now we only support v3 pbrt files
         downloadDir = fullfile(downloadRoot,'data','v3');
-        baseURL = 'http://web.stanford.edu/people/wandell/data'
+        baseURL = 'http://web.stanford.edu/people/wandell/data';
     case {'hyperspectral', 'multispectral', 'hdr'}
-        baseURL = 'http://web.stanford.edu/people/david81'
+        % How do we set the downloadDir in this case?
+        baseURL = 'http://web.stanford.edu/people/david81';
     otherwise
-        error('not supported yet');
+        error('sceneType %s not supported.',sceneType);
 end
 
-downloadFName = fullfile(downloadDir, sceneName);
+%% We have the download directory, local name, and remote name
 
-websave(downloadFName,[baseURL,remoteFileName])
+%  Check if just a listing. In this case we need the scene type, only
+if list
+    % Do something
+    return;
+else
+    % Donwload
+    downloadFName = fullfile(downloadDir, localName);
+    websave(downloadFName,[baseURL,remoteName])
+end
+
+%%
+if verbose
+    disp('Tell the user what happened');
+end
 
 % chdir(fullfile(isetbioRootPath,'local'));
-websave('chessSet.zip','http://web.stanford.edu/people/wandell/data/chessSet.zip');
+% websave('chessSet.zip','http://web.stanford.edu/people/wandell/data/chessSet.zip');
 
 end
