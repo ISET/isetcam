b0876cf04b6407c400266b083314db98e340bf41
David Cardinal <github@proshooters.com>
2021-01-31 15:54:11 -0800

    First pass at renaming to cp

---
+++ ci/cpBurstCamera.m
@@ -0,0 +1,113 @@
+classdef cpBurstCamera < cpCamera
+    %CIBURSTCAMERA Sub-class of ciCamera that adds burst and bracketing
+    %   Basic multi-capture functionality, to be used for testing
+    %   and as a base class for additional enhancements
+    
+    % History:
+    %   Initial Version: D.Cardinal 12/2020
+
+    properties
+        numHDRFrames = 3;
+        numBurstFrames = 3;
+        numFocusFrames = 5; 
+    end
+    
+    methods
+        function obj = ciBurstCamera()
+            %CIBURSTCAMERA Construct an instance of this class
+            obj.cmodules(1) = ciCModule(); % 1 (or more) CModules
+            obj.isp = ciBurstIP();     % extended ip
+            obj.supportedIntents = {'Auto', 'HDR', 'Burst', 'Focus'};
+            
+        end
+        
+       function ourPicture = TakePicture(obj, aCIScene, intent, options, camProps)
+           
+           arguments
+               obj;
+               aCIScene;
+               intent;
+               options.numHDRFrames = 3;
+               options.numBurstFrames = 3;
+               options.numFocusFrames = 3;
+               options.returnIP = obj.isp.returnIP;
+               camProps.?ciCamera;
+               camProps.imageName char = '';
+               camProps.reRender (1,1) {islogical} = true;
+           end
+           if ~isempty(camProps.imageName)
+               obj.isp.ip = ipSet(obj.isp.ip, 'name', camProps.imageName);
+           end
+           obj.numHDRFrames = options.numHDRFrames;
+           obj.numBurstFrames = options.numBurstFrames;
+           obj.numFocusFrames = options.numFocusFrames;
+
+           
+           varargin=namedargs2cell(camProps); 
+           switch intent
+               case 'FocusStack'
+                   stackFrames = obj.numFocusFrames;
+               otherwise
+                   stackFrames = 0;
+           end
+           ourPicture = TakePicture@ciCamera(obj, aCIScene, intent, ...,
+               'returnIP', options.returnIP, 'stackFrames', stackFrames, varargin{:});
+           
+       end
+       
+       % Decides on number of frames and their exposure times
+       % based on the preview image passed in from the camera module
+       function [expTimes] = planCaptures(obj, previewImages, intent)
+
+           if isequal(previewImages{1}.type, 'opticalimage')
+               oi = previewImages{1};
+           else
+               oi = oiCompute(previewImages{1},obj.cmodules(1).oi);
+           end
+           % by default set our base exposure to simple auto-exposure
+           baseExposure = [autoExposure(oi, obj.cmodules(1).sensor)];
+
+          
+           switch intent
+               case 'HDR'
+                   % Bracket the requested number of frames around our base
+                   % exposure. 
+                   % TODO: An AutoHDR based on overall
+                   % scene dynamic range.
+                   numFrames = obj.numHDRFrames;
+                   frameOffset = (numFrames -1) / 2;
+                   if numFrames > 1 && ~isodd(numFrames)
+                       numFrames = numFrames + 1;
+                   end
+                   expTimes = repmat(baseExposure, 1, numFrames);
+                   expTimes = expTimes.*(2.^[-1*frameOffset:1:frameOffset]);
+               case {'Burst', 'FocusStack'}
+                   % For now this is a very simple algorithm that just
+                   % takes the base exposure and divides it into the number
+                   % of frames.
+                   switch intent
+                       case 'Burst'
+                           numFrames = obj.numBurstFrames;
+                       case 'FocusStack'
+                           numFrames = obj.numFocusFrames;
+                   end
+                   % Future: Algorithm here to calculate number of images and
+                   % exposure time based on estimated processing power,
+                   % lighting, and possibly motion/intent
+                   expTimes = repmat(baseExposure/numFrames, 1, numFrames);
+                                                        
+               otherwise
+                   % just do what the base camera class would do
+                   [expTimes] = planCaptures@ciCamera(obj, previewImages, intent);
+           end
+       end
+              
+       function infoArray = showInfo(obj)
+           infoArray = showInfo@ciCamera(obj);
+           infoArray = [infoArray; {'HDR Frames', obj.numHDRFrames}];
+           infoArray = [infoArray; {'Burst Frames', obj.numBurstFrames}];
+           infoArray = [infoArray; {'Focus Frames', obj.numFocusFrames}];
+       end
+    end
+end
+
---
+++ ci/cpCModule.m
@@ -0,0 +1,118 @@
+classdef cpCModule
+    %cpCModule Camera module including sensor and optics
+    %   wraps existing sensor and oi structs to allow
+    %   control as a unit by a computational camera system
+    %   initially we're supporting 1 cpCModule per ciCamera,
+    %   but as many camera systems now use several, we expect
+    %   to support that in future.
+    %
+    %   Note that if we are working with pbrt & a lens file
+    %   then the CModule gets back an oi and ignores its own optics.
+    
+    % History:
+    %   Initial Version: D.Cardinal 12/2020
+    
+    properties
+        oi; % optics plus their resulting image
+        sensor;
+        stabilizer = []; % for future use :)
+        % for future, do user filters go here or in optics?
+    end
+    
+    % Currently, many of the calls to us are just going to be passthrough
+    % to either the sensor or optics structs, but not sure there is a
+    % better way to deal with it that leaves existing code alone.
+    
+    methods
+        function obj = cpCModule(options)
+            %cpCModule Construct an instance of this class
+            arguments
+                % if we don't get sensor or oi passed in, use default
+                options.sensor = sensorCreate();
+                options.oi = oiCreate();
+            end
+            obj.sensor = options.sensor;
+            obj.oi = options.oi;
+            
+        end
+        
+        function cOutput = compute(obj, sceneArray, exposureTimes, options) % will support more args
+            %COMPUTE Calculate what we capture
+            %   Simplest case this is sensorCompute + oiCompute
+            %   however, we also integrate multi-capture
+            %   including support for scenes that change during the
+            %   burst or HDR capture
+            
+            %   For burst/bracket we (or our caller) use CScene to call PBRT (if we have an
+            %   appropriate scene) to generate a set of scenes for us. Or
+            %   we synthetically approximate motion from either a baseline
+            %   ISET scene or image file. If given a burst of scenes or
+            %   images, we assume they already incorporate motion.
+            %
+            %   TODO: If we load our lens model into PBRT, then it gives us
+            %   an array of oi's, not scenes
+            
+            arguments
+                obj cpCModule;
+                sceneArray;
+                exposureTimes;
+                options.stackFrames {mustBeNumeric} = 0;
+            end
+            
+            cOutput = [];
+            for ii = 1:numel(sceneArray)
+                
+                ourScene = sceneArray{ii};
+                if strcmp(ourScene.type, 'scene')
+                    % DOESN'T WORK. Clearly doing something wrong:( DJC
+                    if options.stackFrames > 0
+                        % we use defocus from depth here, especially needed
+                        % for focus stacking if not done in pbrt. Need to
+                        % add a way to pass focal distances.
+                        % [oi, oiD, D] = s3dRenderDepthDefocus(scene, oi, imgPlaneDist, depthEdges, cAberration)
+                        imgPlaneDist = opticsGet(obj.oi.optics,'focal length');
+                        multiplier = 1.2; % go extreme to see effect
+                        for iii = 1:options.stackFrames
+                            [opticalImage, ~, ~] = s3dRenderDepthDefocus(ourScene, obj.oi, imgPlaneDist);
+                            imgPlaneDist = imgPlaneDist*multiplier;
+                            % set sensor FOV to match scene.
+                            sceneFOV = [sceneGet(ourScene,'fovhorizontal') sceneGet(ourScene,'fovvertical')];
+                            obj.sensor = sensorSetSizeToFOV(obj.sensor,sceneFOV,opticalImage);
+                            oiWindow(opticalImage); % Check to see what they look like!
+                        end
+                    else
+                        opticalImage = oiCompute(obj.oi, ourScene);
+                        % set sensor FOV to match scene, but only once or
+                        % we get multiple sizes, which don't merge
+                        if ii == 1
+                            sceneFOV = [sceneGet(ourScene,'fovhorizontal') sceneGet(ourScene,'fovvertical')];
+                            obj.sensor = sensorSetSizeToFOV(obj.sensor,sceneFOV,opticalImage);
+                        end
+                    end
+                    
+                elseif strcmp(ourScene.type, 'oi') || strcmp(ourScene.type, 'opticalimage')
+                    % we already used a lens, so we got back an OI
+                    opticalImage = ourScene;
+                    % this gets really broken, as our sensor shows a tiny
+                    % FOV, so set size makes it massive resolution???'
+                    oiFOV = [oiGet(opticalImage,'hfov'), oiGet(opticalImage,'vfov')];
+                    obj.sensor = sensorSetSizeToFOV(obj.sensor,oiFOV,opticalImage);
+                else
+                    error("Unknown scene render");
+                end
+                obj.sensor = sensorSet(obj.sensor, 'exposure time', exposureTimes(ii));
+                % The OI returned from pbrt sometimes doesn't currently give us a
+                % width or height, so we need to make something up:
+                %% Hack -- DJC
+                oiAngularWidth = oiGet(opticalImage,'wangular');
+                if isempty(oiAngularWidth)
+                    warning("No idea why we need to set wangular here!");
+                    opticalImage = oiSet(opticalImage, 'wangular', .30);
+                end
+                sensorImage = sensorCompute(obj.sensor, opticalImage);
+                cOutput = [cOutput sensorImage]; %#ok<AGROW>
+            end
+        end
+    end
+end
+
