0b6a52ed212e6f31d71367053467a57cb4a4038a
David Cardinal <github@proshooters.com>
2021-02-08 14:52:59 -0800

    more comments

--- cp/s_cpAssembleScene.m
+++ cp/s_cpAssembleScene.m
@@ -1,3 +1,11 @@
+% sample script to assemble a pbrt test scene for object and camera motion.
+% 
+% Feb, 2021
+%
+% We load the "bare" Cornell box, and then (in theory) add the Stanford
+% bunny and the MCC chart (and AF target?) so that we have a full test
+% scene.
+%
 ieInit();
 filmResolution = 256;
 numRays = 64;
@@ -16,5 +24,17 @@ ourCamera.cmodules(1).sensor = ...
     sensorSet(ourCamera.cmodules(1).sensor, ...
     'size', [filmResolution, filmResolution]);
 
+% Look at the original scene by showing our camera and then clicking
+% "Preview" to get a rendering.
 cpCameraWindow(ourCamera, simpleScene);
+
+% Next:
+
+% Add Bunny
+
+% Add MCC
+
+% Add AF chart if there is an asset for it
+
+% Give the resulting scene to our camera & try again.
 
