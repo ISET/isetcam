b93da9aeb890930107757d6b7b9c1b6dff72383c
Brian Wandell <wandell@stanford.edu>
2021-02-08 17:59:42 -0800

    Loading the bunny asset

--- cp/s_cpAssembleScene.m
+++ cp/s_cpAssembleScene.m
@@ -1,11 +1,13 @@
 % sample script to assemble a pbrt test scene for object and camera motion.
-% 
+%
 % Feb, 2021
 %
 % We load the "bare" Cornell box, and then (in theory) add the Stanford
 % bunny and the MCC chart (and AF target?) so that we have a full test
 % scene.
 %
+
+%%
 ieInit();
 filmResolution = 256;
 numRays = 64;
@@ -14,27 +16,41 @@ ourCamera = cpBurstCamera();
 sensor = sensorFromFile('ar0132atSensorRGB');
 ourCamera.cmodules(1) = cpCModule('sensor', sensor);
 
-% scene with no lens file (yet)
-simpleScene = cpScene('pbrt', 'scenePath', 'CornellBoxReference', ...,
+%% scene with no lens file (yet)
+simpleScene = cpScene('pbrt', 'scenePath', 'CornellBoxReference', ...
     'sceneName', 'CornellBoxReference', ...
     'resolution', [filmResolution filmResolution], ...
-    'numRays', numRays, 'sceneLuminance', sceneLuminance);
+    'numRays', numRays, ...
+    'sceneLuminance', sceneLuminance);
 
-ourCamera.cmodules(1).sensor = ...
-    sensorSet(ourCamera.cmodules(1).sensor, ...
-    'size', [filmResolution, filmResolution]);
 
-% Look at the original scene by showing our camera and then clicking
-% "Preview" to get a rendering.
-cpCameraWindow(ourCamera, simpleScene);
-
-% Next:
-
-% Add Bunny
-
-% Add MCC
-
-% Add AF chart if there is an asset for it
-
-% Give the resulting scene to our camera & try again.
-
+%{
+    thisR = simpleScene.thisR;
+    bunny = load('bunny.mat');
+    thisR.set('asset',bunny.assetTree.Node{1},'add',1);
+    thisR.assets.show;
+    thisR.set('material', 'add', bunny.matList{1});
+    piWrite(thisR);
+    scene = piRender(thisR);
+    sceneWindow(scene);
+    %}
+    
+    ourCamera.cmodules(1).sensor = ...
+        sensorSet(ourCamera.cmodules(1).sensor, ...
+        'size', [filmResolution, filmResolution]);
+    
+    % Look at the original scene by showing our camera and then clicking
+    % "Preview" to get a rendering.
+    cpCameraWindow(ourCamera, simpleScene);
+    
+    % Next:
+    
+    % Add Bunny
+    
+    % Add MCC
+    
+    % Add AF chart if there is an asset for it
+    
+    % Give the resulting scene to our camera & try again.
+    
+    %%
