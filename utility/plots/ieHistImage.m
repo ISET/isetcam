4995e20a9a2303ed17506bada57c1a190df84e54
Brian Wandell <wandell@stanford.edu>
2021-11-04 17:30:20 -0700

    Update ieHistImage.m
    Added parameters and an example

--- utility/plots/ieHistImage.m
+++ utility/plots/ieHistImage.m
@@ -1,35 +1,60 @@
-function [img, fhandle] = ieHistImage(X, plotFlag, fhandle)
+function [img, fhandle] = ieHistImage(X, varargin)
 % Create an intensity histogram from the (x,y) data in columns of X
 %
 % Synopsis
 %    [img, figHandle] = ieHistImage(X, [plotFlag = true], fhandle)
 %
-% X:  An N x 2 matrix of the scatter plot values.  These are divided into
-%     32 bins at the moment.
-%     We need to add arguments to set the number and values of the histogram edges
+% Inputs:
+%   X:  An N x 2 matrix of the scatter plot values.  We need to add
+%     arguments to set the number and values of the histogram edges 
 %
-% pair of param/val arguments might be
-%    edge1    vector
-%    edge2    vector
-%    showPlot (true/false)
+% Optional key/val pairs
+%   plotFlag - logical, default true
+%   fhandle  - Figure handle
+%   pair of param/val arguments might be
+%     edge1    vector
+%     edge2    vector
 %
-% BW, Copyright Imageval Consulting, LLC, 2015
+% Outputs
+%   img - Image histogram
+%   fhandle - Matlab figure handle
 %
 % See also
 %
 
+% Examples:
+%{
+  X    = [1:1024,2000:10:2100]; a = 1; b = 0.01;
+  Y    = a*X + b; 
+  Y    = Y + randn(size(Y))*64;
+  edges = [64,64];
+  imgH = ieHistImage([X(:),Y(:)],'edges',edges);
+  colormap([0.5 0.5 0.5; hot]); grid on;
+  g = fspecial('gaussian',[8,8]);
+  imgHG = conv2(imgH,g,'same');
+  ieNewGraphWin; imagesc(imgHG); axis xy; grid on; axis square
+%}
+
 %% Check parameters
-if ieNotDefined('X'),  error('X required');
-elseif size(X,2) ~= 2, error('X size is wrong');
-end
-if ieNotDefined('plotFlag'), plotFlag = true; end
-if ieNotDefined('fhandle'), fhandle = []; end
+p = inputParser;
+varargin = ieParamFormat(varargin);
+
+p.addRequired('X',@(x)(size(x,2) == 2));
+p.addParameter('plotflag',true,@islogical);
+p.addParameter('fhandle',[],@(x)(isequal(class(x),'matlab.ui.Figure')));
+p.addParameter('edges',[32,32],@isvector);
+
+p.parse(X,varargin{:});
+
+plotFlag = p.Results.plotflag;
+fhandle  = p.Results.fhandle;
+edges    = p.Results.edges;
 
 %% Calls external function histcn to form the image
 
 % Do the calculation
-[img, ~, mid] = histcn(X);
-% vcNewGraphWin; plot(X(:,1),X(:,2),'.'); axis equal; identityLine;
+[img, ~, mid] = histcn(X,edges(1),edges(2));
+% ieNewGraphWin; plot(X(:,1),X(:,2),'.'); axis equal; identityLine;
 
 %% We will allow more parameters here
 if plotFlag
