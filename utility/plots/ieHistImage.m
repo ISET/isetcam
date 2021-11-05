7819a921ccf64fe81dfe18ef0e986374b42d60b7
Brian Wandell <wandell@stanford.edu>
2021-11-04 21:54:27 -0700

    Improved image histogram methods

--- utility/plots/ieHistImage.m
+++ utility/plots/ieHistImage.m
@@ -1,38 +1,44 @@
-function [img, fhandle] = ieHistImage(X, varargin)
-% Create an intensity histogram from the (x,y) data in columns of X
+function [img, xy, cmap, fhandle] = ieHistImage(X, varargin)
+% Create an intensity histogram from the (x,y) data in two columns of X
 %
 % Synopsis
-%    [img, figHandle] = ieHistImage(X, [plotFlag = true], fhandle)
+%    [img, xy, figHandle] = ieHistImage(X, [plotFlag = true], fhandle)
 %
 % Inputs:
-%   X:  An N x 2 matrix of the scatter plot values.  We need to add
-%     arguments to set the number and values of the histogram edges 
+%   X:  An N x 2 matrix of the scatter plot values.
 %
 % Optional key/val pairs
+%   method   - Use histcn or scatplot drawing method (default: scatplot)
 %   plotFlag - logical, default true
 %   fhandle  - Figure handle
-%   pair of param/val arguments might be
-%     edge1    vector
-%     edge2    vector
+%   edges    - Number of edges on x and y axes 
 %
 % Outputs
-%   img - Image histogram
+%   img     - Image histogram
+%   xy      - Cell array x and y coordinates of the image
+%   cmap    - Color map used in ieHistImage
 %   fhandle - Matlab figure handle
 %
 % See also
-%
+%   scatplot, histcn,v_extHistcn
 
 % Examples:
 %{
   X    = [1:1024,2000:10:2100]; a = 1; b = 0.01;
-  Y    = a*X + b; 
+  Y    = a*X + b;
   Y    = Y + randn(size(Y))*64;
-  edges = [64,64];
-  imgH = ieHistImage([X(:),Y(:)],'edges',edges);
-  colormap([0.5 0.5 0.5; hot]); grid on;
+  edges = [64,32];
+  [imgH, xy, cmap] = ieHistImage([X(:),Y(:)],'hist type','histcn','edges',edges,'plot flag',true);
   g = fspecial('gaussian',[8,8]);
   imgHG = conv2(imgH,g,'same');
-  ieNewGraphWin; imagesc(imgHG); axis xy; grid on; axis square
+  ieNewGraphWin; imagesc(imgHG); colormap(cmap); axis xy; grid on; axis square
+%}
+%{
+  X    = [1:1024,2000:1:2100]; a = 1; b = 0.01;
+  Y    = a*X + b;
+  Y    = Y + randn(size(Y))*64;
+  imgH = ieHistImage([X(:),Y(:)],'hist type','scatplot','scatmethod','voronoi');
+  identityLine;
 %}
 
 %% Check parameters
@@ -43,7 +49,10 @@ p.addRequired('X',@(x)(size(x,2) == 2));
 p.addParameter('plotflag',true,@islogical);
 p.addParameter('fhandle',[],@(x)(isequal(class(x),'matlab.ui.Figure')));
 p.addParameter('edges',[32,32],@isvector);
+p.addParameter('histtype','scatplot',@ischar);
 
+p.addParameter('scatmethod','',@ischar);
+p.addParameter('scatradius',8,@isnumeric);
 p.parse(X,varargin{:});
 
 plotFlag = p.Results.plotflag;
@@ -51,10 +60,30 @@ fhandle  = p.Results.fhandle;
 edges    = p.Results.edges;
 
 %% Calls external function histcn to form the image
-
-% Do the calculation
-[img, ~, mid] = histcn(X,edges(1),edges(2));
-% ieNewGraphWin; plot(X(:,1),X(:,2),'.'); axis equal; identityLine;
+switch p.Results.histtype
+    case 'histcn'
+        % Do the calculation.  
+        % The image and the x,y values of the image ('mid')
+        [img, ~, tmp] = histcn(X,edges(1),edges(2));
+        xy{1} = tmp{1,1};
+        xy{2} = tmp{1,2};
+        % ieNewGraphWin; plot(X(:,1),X(:,2),'.'); axis equal; identityLine;
+        
+    case 'scatplot'
+        x = X(:,1); y = X(:,2);
+        method = 'voronoi';
+        radius = sqrt((range(x)/30)^2 + (range(y)/30)^2);
+        N = 100;  % Not sure
+        n = 5;    % Smoothing?
+        po = 1;   % Plot options
+        ms = 4;   % Marker size        
+        scatStruct = scatplot(x,y,method,radius,N,n,po,ms,gray(256));
+        img = scatStruct.zif;
+        xy{1} = scatStruct.xi(1,:);
+        xy{2} = scatStruct.yi(:,1);
+    otherwise
+        error('Unknown histogram method %s.',p.Results.histtype);
+end
 
 %% We will allow more parameters here
 if plotFlag
@@ -62,8 +91,11 @@ if plotFlag
     else,                figure(fhandle)
     end
     
-    imagesc(mid{1:2},img);
-    axis xy; colormap(0.4 + 0.6*gray(256)); colorbar
+    imagesc(xy{1},xy{2},img);
+    
+    axis xy; cmap = 0.4 + 0.6*gray(256);
+    colormap(0.4 + 0.6*gray(256)); colorbar
+    grid on; axis square;
 end
 
 end
