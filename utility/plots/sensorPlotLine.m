060b6d5a3423185d1d10d2dd21ab15d8836bf83c
Brian Wandell <wandell@stanford.edu>
2021-11-06 16:19:47 -0700

    Comments on plotting
    We changed the monochrome sensor uData slots to match the color sensor uData slots.  I hope this doesn't screw up the validation routines.  Will check shortly.

--- utility/plots/sensorPlotLine.m
+++ utility/plots/sensorPlotLine.m
@@ -94,7 +94,7 @@ if nSensors > 1
     end
     uData = plotColorISALines(xy,pos,data,ori,nSensors,dataType,sORt,fColors,figNum);
 elseif nSensors == 1
-    figNum = vcNewGraphWin;
+    figNum = ieNewGraphWin;
     uData = plotMonochromeISALines(xy,pos,data,ori,dataType,sORt,figNum);
 end
 
@@ -234,7 +234,7 @@ switch lower(sORt)
         ystr = sprintf('%s',dataType); ylabel(ystr);
         tickLocs = ieChooseTickMarks(pos);
         set(gca,'xtick',tickLocs);
-        grid on; uData.pos = pos;uData.data = data;
+        grid on; uData.pixPos = pos;uData.pixData = data;
         
     case {'transform','fourier','fourierdomain','fft'}
         % The data come in specified in microns.  We convert to millimeters
