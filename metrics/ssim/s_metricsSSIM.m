561825f5898b2e4bc7b03b8b6848e5476dc092b0
Brian Wandell <wandell@stanford.edu>
2022-11-30 21:30:58 -0800

    Update s_metricsSSIM.m

--- metrics/ssim/s_metricsSSIM.m
+++ metrics/ssim/s_metricsSSIM.m
@@ -3,13 +3,56 @@
 % Scratch for now.  More to come.
 
 
+%% Reference scene
 scene = sceneCreate('macbeth',64);
 sceneWindow(scene);
 
 ref = sceneGet(scene,'rgb');
+lumRef = sceneGet(scene,'luminance');
+
+ieNewGraphWin; imshow(ref);
+ieNewGraphWin; imagesc(lumRef); colormap("gray"); axis image
+
+%%  Noisy scene
+
+energy = sceneGet(scene,'energy');
+sz = sceneGet(scene,'size');
+nWave = sceneGet(scene,'n wave');
+noise = randn(sz(1),sz(2),nWave);
+
+% The noise has a mean close to zero
+noise = noise .* ((energy.^0.5)*0.05);
+
+scene2 = sceneSet(scene,'energy',energy + noise);
+sceneWindow(scene2);
+
+test = sceneGet(scene2,'rgb');
+lumTest = sceneGet(scene2,'luminance');
+
+ieNewGraphWin; imshow(test);
+ieNewGraphWin; imagesc(lumTest); colormap("gray"); axis image
+
+%%
+montage({test,ref});
+montage({lumTest,lumRef});
+
+
+%%
+ieNewGraphWin;
+plot(test(1:10:end),ref(1:10:end),'.');
+
+%%
+
+mean(test(:) - photons(:))
 
 test = ref + 0.1*randn(size(ref));
 test(test>1) = 1;
+ieNewGraphWin; imshow(test);
+
+%%  Compute the luminance
+
+
+
 
 [val,ssimmap] = ssim(test,ref);
 
