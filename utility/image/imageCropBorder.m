fc86134568ac81d0fd447d99fd1223179db7d08d
David Cardinal <github@cardinalphoto.com>
2023-03-09 08:41:59 -0800

    fix error message

--- utility/image/imageCropBorder.m
+++ utility/image/imageCropBorder.m
@@ -19,9 +19,22 @@ col1 = min(c);
 col2 = max(c);
 
 % crop if we got results
+<<<<<<< Updated upstream
 if (row2 > row1) && (col2 > col1)
     croppedImage = img(row1:row2, col1:col2, :);
 else
     croppedImage = img;
+=======
+try
+    % sometimes we get an error, but haven't pinned it down
+    if (row2 > row1) && (col2 > col1)
+        croppedImage = img(row1:row2, col1:col2, :);
+    else
+        croppedImage = img;
+    end
+catch ME
+    disp('Error Message:')
+    disp(ME.message)
+>>>>>>> Stashed changes
 end
 
