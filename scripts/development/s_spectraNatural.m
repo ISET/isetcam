9bf6f32ef4c3f83bf7a3ea7d30fbdf6ce11114d5
Brian Wandell <wandell@stanford.edu>
2023-05-15 10:03:57 -0700

    Update s_spectraNatural.m

--- scripts/development/s_spectraNatural.m
+++ scripts/development/s_spectraNatural.m
@@ -2,8 +2,8 @@
 % ISETAuto day
 %
 % Convert the spectra to Natural Spectra principal components, but
-% making sure that the new spectra impact a camera the same way that
-% the old spectra would.
+% making sure that the new spectra impact a camera and the human cones
+% the same way that the old spectra would.
 %
 % Or maybe preserve a couple of cameras?
 %
@@ -13,13 +13,19 @@
 
 %% Write a function to load up a bunch of reflectances
 
-% What did Zheng contribute here?  Can I use that?
+% What did Zheng implement here?  Can I use that?
+% Probably I should load up a bunch of surfaces here, not the basis,
+% and then a bunch of lights.  But I think this is probably formally
+% equivalent if I multiply the basis functions by the lights and then
+% take the top 6 or 8 principal components, matching the number of
+% sensors we design.
 wave = 400:10:700;
-rBasis = ieReadSpectra('reflectanceBasis',wave);
+[rBasis,~,comment] = ieReadSpectra('reflectanceBasis',wave);
 
 %% Multiply the reflectances through a number of different lights
 
 % Write a function to load up a bunch of light spectra
+% Combine the surfaces
 %
 d50 = ieReadSpectra('D50',wave);
 d65 = ieReadSpectra('D65',wave);
@@ -31,45 +37,64 @@ ieNewGraphWin;
 plotRadiance(wave,d75);
 
 %% Load up a couple of sensor spectral files
-rgbw = ieReadSpectra('RGBW',wave);
-rgbw = ieScale(rgbw,0,1);
 
-cmyg = ieReadSpectra('CMYG',wave);
-cmyg = ieScale(cmyg,0,1);
+% A camera we used in the simulation
+rgb = ieReadSpectra('RGB',wave);
+rgb = ieScale(rgb,0,1);
 
-foveon = ieReadSpectra('Foveon',wave);
-foveon = ieScale(foveon,0,1);
-C = [rgbw,cmyg,foveon];
+% The human visual system
+cones = ieReadSpectra('Stockman',wave);
+cones = ieScale(cones,0,1);
+C = [rgb,cones];
+
+% svd(C)
+
+% Maybe we should choose n spectral bands to make the weight estimates
+% unique.
+cmy = ieReadSpectra('CMY',wave);
+cmy = ieScale(cmy,0,1);
+
+C = [rgb,cmy,cones];
+% svd(C)
 
 plotRadiance(wave,C);
 
 %% We want to find weights, w, for a radiance such that
 %
-% rgbw' * s = rgbw'* rBasis * w
+% C' * s = C'* rBasis * w
 %
-% Suppose rgbw is 31 x 4, and rBasis is 31 x 8
-% then X = rgbw' * rBasis is 4 x 8
+% Suppose C is 31 x 9, and rBasis is 31 x 8
+% then 
 %
-% (X' * X) is 8 x 8, but it is probably singular
+% X = C' * rBasis 
+% 
+% is 9 x 8
+%
+% 
+% (X' * X) is 8 x 8 and close to singular.  But probably usable.
+%
 X = C'*rBasis;
 svd(X'*X)
 
-%% So now, we have enough sensor constraints to find a unique w in the natural spectral regime
+%% We choose enough channels to find a unique set of weights for the natural spectra 
 %
-%  s is known
+%  s is known, w are the weights
 %
 %  C'*s = C'*rBasis*w
+%  X = C'*rBasis
+%
 %  C'*s = X * w
 %  X'*C'*s = X'*X * w
 %  inv(X'*X)*X'*s = w
 %
-% As long as X'*X is invertible, we should be OK
+% We need X'*X to be invertible, we should be OK.  We should use the
+% divide (\) for estimating, rather than inv(), I believe.
 %
-% The new spectral radiance is rBasis*w.  By construction it is
-% natural, and by construction it matches the responses in those
-% different camera sensors
+% When w is estimated, the spectral radiance is rBasis*w.  By
+% construction it is natural, and by construction it matches the
+% responses in the different channels.
 %
-% 
+% We might consider matching channels in 8 spectral bands, by the way.
 
 
 %% End
