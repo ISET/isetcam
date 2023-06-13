687d3bfea5b1c98423423b3b763e6e1798c95d77
David Cardinal <github@cardinalphoto.com>
2023-06-13 06:16:54 -0700

    working version

--- scene/sceneMakeVideo.m
+++ scene/sceneMakeVideo.m
@@ -1,12 +1,24 @@
 function [videoFile] = sceneMakeVideo(sceneList)
 %SCENEMAKEVIDEO Make a video from a cell array of scenes
 
+% video structure with frames for creating clips
+ourVideo = struct('cdata',[],'colormap',[]);
+
 % Initialize a videowriter
-% ...
+videoFile = strcat("Auto-Video", "-", 'output');
+v = VideoWriter(videoFile, 'MPEG-4');
+v.FrameRate = 3; % 
+
+open(v);
+
 for ii = 1:numel(sceneList)
     % Get an image, but don't show
-    im = sceneShowImage(sceneList{ii}, -1, 2.2);
-    imFrame = im2frame(im)
-    
+    im = sceneShowImage(sceneList{1,ii}, -3, 2.2);
+    ourVideo(ii) = im2frame(im);
+
 end
+
+writeVideo(v, ourVideo);
+
+close(v);
 
