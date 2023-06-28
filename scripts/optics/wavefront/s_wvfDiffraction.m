34d650490075a75351900f74e6fa00e7eb7fbf25
Brian Wandell <wandell@stanford.edu>
2023-06-28 16:15:31 -0700

    Test wvf diffraction with LCA calculations

--- scripts/optics/wavefront/s_wvfDiffraction.m
+++ scripts/optics/wavefront/s_wvfDiffraction.m
@@ -1,5 +1,10 @@
 %% Create different diffraction limited cases
-
+%
+% Include human LCA and not
+% Different pupil sizes
+%
+% See also
+%  
 %%
 ieInit;
 
@@ -11,16 +16,16 @@ thisWave = 600;
 wvf = wvfSet(wvf,'measured pupil diameter',20);
 wvf = wvfSet(wvf,'focal length',flengthM);
 
-% This way, no LCA included
+%% Measured and calc waves differ, but no LCA in this section
 wvf = wvfSet(wvf,'calc wave',thisWave);
-wvf = wvfSet(wvf,'measured wave',thisWave);
+wvf = wvfSet(wvf,'measured wave',550);
 
 % BW:  We should be able to specify the LCA, not only use human.
 pupilMM = [1, 3, 6, 9];
 for pp = pupilMM
     
     wvf = wvfSet(wvf,'calc pupil diameter',pp);
-    wvf = wvfComputePSF(wvf,'lca',false);
+    wvf = wvfComputePSF(wvf,'lca',false,'force',true);
     wvfPlot(wvf,'image psf space','um',thisWave,20,'airy disk');
     fNumber = wvfGet(wvf,'fnumber');
     AD = airyDisk(thisWave,fNumber,'units','um','diameter',true);
@@ -28,4 +33,27 @@ for pp = pupilMM
 
 end
 
-%%
+%% Measured and calc waves differ, with LCA
+
+pupilMM = [1, 3, 6, 9];
+for pp = pupilMM
+    
+    wvf = wvfSet(wvf,'calc pupil diameter',pp);
+    wvf = wvfComputePSF(wvf,'lca',true,'force',true);
+    wvfPlot(wvf,'image psf space','um',thisWave,20,'airy disk');
+    fNumber = wvfGet(wvf,'fnumber');
+    AD = airyDisk(thisWave,fNumber,'units','um','diameter',true);
+    title(sprintf('Wave %.2f Airy %.2f',pp,AD));
+
+end
+%% Series of wavelengths with human LCA
+
+wvf = wvfSet(wvf,'calc pupil diameter',3);
+for ww = 400:50:700
+    wvf = wvfSet(wvf,'calc wave',ww);
+    wvf = wvfComputePSF(wvf,'lca',true,'force',true);
+    wvfPlot(wvf,'image psf space','um',ww,20,'airy disk');
+    fNumber = wvfGet(wvf,'fnumber');
+    AD = airyDisk(ww,fNumber,'units','um','diameter',true);
+    title(sprintf('Pupil %.2f Airy %.2f',ww,AD));
+end
