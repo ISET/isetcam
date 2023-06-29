c8d2d5b373f9d48547dfb207f902adb033751649
Brian Wandell <wandell@stanford.edu>
2023-06-28 20:55:18 -0700

    Update sceneHDRLights.m
    adding back the parameters

--- scene/pattern/sceneHDRLights.m
+++ scene/pattern/sceneHDRLights.m
@@ -17,39 +17,62 @@ function scene = sceneHDRLights(varargin)
   scene = sceneHDRLights();
   sceneWindow(scene);
 %}
+%{
+  scene = sceneHDRLights('n circles',4,'radius',repmat(0.01,1,4),'circle colors',{'white'});
+  sceneWindow(scene);
+%}
 
 %%
+varargin = ieParamFormat(varargin);
 p = inputParser;
 
-imSize = 384;
+p.addParameter('imagesize',384,@isnumeric);
+
+p.addParameter('ncircles',4,@isnumeric);
+p.addParameter('radius',[0.01,0.035,0.07,0.1],@isvector);
+p.addParameter('circlecolors',{'white','green','blue','yellow','magenta','white'},@iscell);
+
+p.addParameter('nlines',4,@isnumeric);
+p.addParameter('linelength',0.02,@isnumeric);
+p.addParameter('linecolors',{'white','green','blue','yellow','magenta','white'},@iscell);
+
+p.parse(varargin{:});
 
 %% Spatial pattern
+imSize   = p.Results.imagesize;
 img = zeros(imSize,imSize);
 
-% Put circles with different sizes across the top third
+%% Put circles with different sizes across the top third
+nCircles = p.Results.ncircles;
+radius   = p.Results.radius;
+cColors  = p.Results.circlecolors;
 y = round(imSize * 0.25);
-xvals = round(linspace(0.2,0.8,4)*imSize);
-radius = [0.02 0.07 0.15 0.2]*imSize/2;
-thisColor = {'white','green','blue','red'};
+xvals = round(linspace(0.2,0.8,nCircles)*imSize);
+radius = radius*imSize;
 for ii = 1:numel(xvals)
-    img = insertShape(img,'filled-circle',[xvals(ii),y,radius(ii)],'Color',thisColor{ii});
+    cc = mod(ii,numel(cColors)) + 1;    
+    img = insertShape(img,'filled-circle',[xvals(ii),y,radius(ii)],'Color',cColors{cc});
 end
 
-% Put lines of different thickness and orientation around the middle
+%% Put lines of different thickness and orientation around the middle
+nLines       = p.Results.nlines;
+lineLength   = p.Results.linelength;
+lColors      = p.Results.linecolors;
+
 y = round(imSize * 0.5);
-xvals = round(linspace(0.1,0.8,6)*imSize);
-thisColor = {'white','green','blue','yellow','magenta','white'};
-
-squareSize = imSize/64;
-hw = round([1,7; 1,3; 2,2; 3,1; 6,1; 8,1]*squareSize);
+xvals = round(linspace(0.1,0.8,nLines)*imSize);
+lineLength = round(lineLength*imSize);
+hw = round([1,7*lineLength; 1,3*lineLength; 3*lineLength,1; 8*lineLength,1]);
 for ii = 1:numel(xvals)
-    img = insertShape(img,'filled-rectangle',[xvals(ii),y,hw(ii,1),hw(ii,2)],'Color',thisColor{ii});
+    cc = mod(ii,numel(lColors)) + 1;
+    img = insertShape(img,'filled-rectangle',[xvals(ii),y,hw(ii,1),hw(ii,2)],'Color',lColors{cc});
 end
 
+%% Squares
 y = round(imSize * 0.75);
 xvals = round(linspace(0.1,0.7,3)*imSize);
-squareSize = imSize/64;
-hw = [2 2; 5 5; 9 9]*squareSize;
+squareEdge = imSize/64;
+hw = [2 2; 5 5; 9 9]*squareEdge;
 for ii = 1:numel(xvals)
     img = insertShape(img,'filled-rectangle',[xvals(ii),y,hw(ii,1),hw(ii,2)],'Color','white');
 end
