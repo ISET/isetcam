1c6130baf75ad167f900207d72eda45253f54236
Brian Wandell <wandell@stanford.edu>
2023-06-29 17:42:00 -0700

    Debugging.  Something not going well with wvf calculations

--- scripts/optics/wavefront/s_wvfDiffraction.m
+++ scripts/optics/wavefront/s_wvfDiffraction.m
@@ -58,4 +58,89 @@ for ww = 400:50:700
     title(sprintf('Pupil %.2f Airy %.2f',ww,AD));
 end
 
+%%  Change the spatial resolution via um per degree.
+
+% This is unfortunate.  It appears that when we change um per degree,
+% we change the spatial spread of the PSF.  
+
+wvf = wvfCreate;    % Default wavefront 5.67 fnumber
+
+flengthM = 7e-3;
+wvf = wvfSet(wvf,'measured pupil diameter',20);
+wvf = wvfSet(wvf,'calc pupil diameter',3);
+wvf = wvfSet(wvf,'focal length',flengthM);
+
+airyDisk(550,wvfGet(wvf,'fnumber'),'diameter',true)
+
+% This does not seem right to me.
+wvf = wvfSet(wvf,'umperdegree',300);
+wvf = wvfComputePSF(wvf);
+wvfPlot(wvf,'2d psf space','um',550,20,'airy disk');
+
+% The mis-match is different if we change this
+wvf = wvfSet(wvf,'umperdegree',150);
+wvf = wvfComputePSF(wvf);
+wvfPlot(wvf,'2d psf space','um',550,20,'airy disk');
+
+% Again.  This starts to look more like Zhenyi's PSF.
+wvf = wvfSet(wvf,'umperdegree',600);
+wvf = wvfComputePSF(wvf);
+wvfPlot(wvf,'2d psf space','um',550,20,'airy disk');
+
+psf = wvfGet(wvf,'psf',550);
+samp = wvfGet(wvf,'psf spatial samples','um',550);
+foo = interp2(samp,samp,psf,0,samp);
+ieNewGraphWin;
+plot(samp,foo,'o');
+grid on;
+set(gca,'xlim',[-20 20]);
+
+%% Not great.
+
+wvf = wvfCreate;    % Default wavefront 5.67 fnumber
+
+flengthM = 7e-3;
+wvf = wvfSet(wvf,'measured pupil diameter',20);
+wvf = wvfSet(wvf,'calc pupil diameter',3);
+wvf = wvfSet(wvf,'focal length',flengthM);
+
+airyDisk(550,wvfGet(wvf,'fnumber'),'diameter',true)
+
+wvf = wvfComputePSF(wvf);
+
+psf = wvfGet(wvf,'psf',550);
+samp = wvfGet(wvf,'psf spatial samples','um',550);
+foo = interp2(samp,samp,psf,0,samp);
+
+lineFig = ieNewGraphWin;
+plot(samp,foo,'o');
+grid on; set(gca,'xlim',[-5 5],'xtick',-5:1:5);
+
+%% This one is like the one above, but shifted slightly negative
+oi = wvf2oi(wvf);
+
+% Show it via OI
+uData = oiPlot(oi,'psf550'); 
+samp = uData.x(1,:);
+foo = interp2(uData.x,uData.y,uData.psf,0,samp);
+
+figure(lineFig);
+hold on; plot(samp,foo,'kx');
+grid on; set(gca,'xlim',[-5 5],'xtick',-5:1:5);
+
+%%  This one is correct.
+
+oi = oiCreate('diffraction limited');
+oi = oiSet(oi,'fnumber',wvfGet(wvf,'fnumber'));
+
+uData = oiPlot(oi,'psf550');
+samp = uData.x(1,:);
+foo = interp2(uData.x,uData.y,uData.psf,0,samp);
+
+figure(lineFig);
+hold on; plot(samp,foo,'o');
+grid on; set(gca,'xlim',[-5 5],'xtick',-5:1:5);
+
 %%
+
+
