7624fe2a7358b15afb5cd9d32c27aa63b842b169
Brian Wandell <wandell@stanford.edu>
2023-07-09 20:23:22 -0700

    Backwards compatibility with ieInWindow. Shadow conePlot fixed

--- gui/ieInWindowMessage.m
+++ gui/ieInWindowMessage.m
@@ -14,6 +14,10 @@ function ieInWindowMessage(str,app,duration)
 %  If duration is not sent, the default is to leave the message in place.
 %  To clear the box, set str = [];
 %
+%  This routine is called slightly differently by the modern appdesigner
+%  methods and the older guide methods.  Retained the older methods for
+%  backwards compatibility (with ISETBio, mainly).
+%
 % Copyright ImagEval Consultants, LLC, 2005.
 
 
@@ -23,12 +27,20 @@ if (~exist('app','var')||isempty(app)),disp(str); return; end
 if (~exist('duration','var')||isempty(duration)),duration = []; end
 
 %% Place the string in the message area.
-app.txtMessage.Text = str;
+if isstruct(app)
+    % Old style window, not appdesigner based.  Here for backwards
+    % compatibility. 
+    app.txtMessage.String = str;
+else
+    app.txtMessage.Text = str;
+end
 
 % If duration is set, replace the string with blank after duration seconds.
 if ~isempty(duration)
     pause(duration);
-    app.txtMessage.Text = '';
+    if isstruct(app), app.txtMessage.String = '';
+    else,             app.txtMessage.Text = '';
+    end
 end
 
 end
