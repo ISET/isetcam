de64075380b346e7ac61c9128e5e829519503f41
Brian Wandell <wandell@stanford.edu>
2023-08-12 14:09:26 -0700

    Update xaxisLine.m

--- utility/plots/xaxisLine.m
+++ utility/plots/xaxisLine.m
@@ -1,33 +1,36 @@
-function thisLine = xaxisLine(ax)
-% Draw a dark line along the x-axis of a graph
+function thisLine = xaxisLine(ax,yval)
+% Draw a dark line parallel to the x-axis of a graph
 %
 % Synopsis
-%   xaxisLine(ax);
+%   xaxisLine(ax,val);
+%
+% Brief description
+%   By default this is along the y=0 axis.  You can choose a different y
+%   value by the 2nd argument
 %
 % Input
-%  axes -   Default is gac
+%  axes -   Default is gca
+%  yval  -  Default is y = 0
 %
 % Output
 %  thisLine - Handle to the plotted line
 %
 % See also
 %   identityLine
-
-
-% Examples:
 %{
-xaxisLine(gca);
+xaxisLine(gca,0.5);
 %}
 
 %%
 if ieNotDefined('ax'), ax = gca; end
+if ieNotDefined('yval'), yval = 0; end
 
 %%
 xlim = get(ax, 'xlim');
 
 % Here's the line from (m1,m1) to (m2,m2).  Both of these points are on
 % the identity line (x = y).
-thisLine = line([xlim(1) xlim(2)], [0 0], 'color', [.3 .3 .3], 'linestyle', '--');
+thisLine = line([xlim(1) xlim(2)], [yval yval], 'color', [.3 .3 .3], 'linestyle', '--');
 
 
 % Set line properties.  These probably want to come in as an argument
