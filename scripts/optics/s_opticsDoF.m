b70ea51ea3e52b63e03661ce02ee47420f77a22c
Brian Wandell <wandell@stanford.edu>
2023-10-06 17:34:37 -0700

    Update s_opticsDoF.m

--- scripts/optics/s_opticsDoF.m
+++ scripts/optics/s_opticsDoF.m
@@ -3,15 +3,23 @@
 % Computing the depth of field for a thin lens with a known focal
 % length and aperture.
 %
-% The wikipedia article and the Levoy notes are helfpul.
-%  https://en.wikipedia.org/wiki/Depth_of_field
+% The depth of field formula is
 %
-% The ISETCam calculation is simple and depends on opticsCoC, which
-% has a wonderful history
+%    DOF = (2 f/# C U^2)/ FocalLength^2
+%
+% The wikipedia article and the Levoy notes are helfpul.
+%
+% Here is the wikipedia article:  https://en.wikipedia.org/wiki/Depth_of_field
+%
+% The ISETCam calculation depends only on the opticsCoC calculation. 
+% The circle of confusion calculation has a wonderful history
+%
 % http://en.wikipedia.org/wiki/Circle_of_confusion#Determining_a_circle_of_confusion_diameter_from_the_object_field
 %
 % See also
 %  s_opticsCoC
+%
+
 
 %%
 ieInit
