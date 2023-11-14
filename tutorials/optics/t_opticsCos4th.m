c605226afb9d4f93b9bc9961e1ebd48e850e75b8
Brian Wandell <wandell@stanford.edu>
2023-11-13 23:00:22 -0800

    Update t_opticsCos4th.m

--- tutorials/optics/t_opticsCos4th.m
+++ tutorials/optics/t_opticsCos4th.m
@@ -7,29 +7,17 @@
 
 %% Large field of view scene
 scene = sceneCreate('uniform d65',512);
-scene = sceneSet(scene,'fov',20);
+scene = sceneSet(scene,'fov',80);
 
 % Diffraction limited shift-invariant optics
 oi = oiCreate('shift invariant');
 focalLength = oiGet(oi,'optics focallength');
-
-height = oiGet(oi,'height','m');
-X = linspace(-height,height,100);
-theta = atand(X/focalLength);
-falloff = cosd(theta).^4'
-ieNewGraphWin; plot(X,falloff)
-
-S = sqrt(focalLength^2 + X.^2);
-ieNewGraphWin; plot(X,(X./S).^4)
 
 oi = oiCompute(oi,scene);
 sz = oiGet(oi,'size');
 
 oiPlot(oi,'illuminance hline',[1,sz(2)/2]);
 oiWindow(oi);
-[uData,fig] = oiPlot(oi,'illuminance hline',[1,20]);
-ax = gca;
-ax.Children.Color = [0 0 0];
 
 
 oi = oiSet(oi,'optics focallength', 4*focalLength);
