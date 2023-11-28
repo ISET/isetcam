b56af997b3aa4b2c611ea3d8e9566bc3f97e0c8f
Brian Wandell <1837145+wandell@users.noreply.github.com>
2023-11-27 18:08:30 -0800

    SSIM and comments on compute

--- metrics/ssim/s_metricsSSIM.m
+++ metrics/ssim/s_metricsSSIM.m
@@ -1,81 +1,82 @@
 %% Experiments with ssim
 %
-% Scratch for now.  More to come.
+% Includes comparisons with S-CIELAB
+%
+% Used for Psych 221 teaching.
 
 
 %%
 ieInit;
 
-%% Reference scene
+%% Reference scene all the way to a rendered image
+
+% The initial calculation has zero noise
 scene = sceneCreate('sweep frequency',512,20);
-sceneWindow(scene);
+% scene = sceneCreate;
+% sceneWindow(scene);
 
-ref = sceneGet(scene,'rgb');
-% refLum = sceneGet(scene,'luminance');
-refLum = sum(ref,3)/3;
+oi = oiCreate;
+oi = oiCompute(oi,scene);
+oi = oiCrop(oi,'border');
 
-%%  Add photon noise
-%
-% scene = sceneAddNoise(scene,varargin)
-%
-% Add Poisson noise to the scene photons
-% Or add Gaussian noise with mean and sd specified in varargin
-%
-photons = sceneGet(scene,'photons');
-sz = sceneGet(scene,'size');
-nWave = sceneGet(scene,'n wave');
-photons2 = photons + randn(sz(1),sz(2),nWave).* photons.*(0.5);
+%% Start with the Sony IMX sesnsor
+sensor = sensorCreate('imx363');
 
-scene2 = sceneSet(scene,'photons',photons2);
-sceneWindow(scene2);
+% Because we turn off all the noise, we need to set the black level to
+% zero.  We should probably do this inside of the sensor set for this
+% special case.  
+sensor = sensorSet(sensor,'noise flag',-1);
+sensor = sensorSet(sensor,'zero level',0);
 
-test = sceneGet(scene2,'rgb');
-% testLum = sceneGet(scene2,'luminance');
+% Adjust the sensor array size and use a long exposure for relatively
+% little photon noise.
+sensor = sensorSet(sensor,'fov',oiGet(oi,'fov'),oi);
+sensor = sensorSet(sensor,'autoexposure',true);
+sensor = sensorCompute(sensor,oi);
+sensorWindow(sensor);
+
+%%
+ip = ipCreate;
+ip = ipSet(ip,'name','default');
+ip = ipSet(ip,'internal cs','XYZ');
+ip = ipSet(ip,'conversion method sensor','MCC Optimized');
+ip = ipSet(ip,'name','MCC-XYZ');
+ip = ipSet(ip,'correction method illuminant','white world');
+ip = ipCompute(ip,sensor);
+ipWindow(ip);
+
+% Here is the reference luminance
+refsRGB = ipGet(ip,'srgb');
+refLum = sum(refsRGB,3)/3;
+
+%% Allow noise and shorten the exposure duration to create a noisy version
+sensor = sensorSet(sensor,'noise flag',2);
+
+% We turn on the noise, so we put back the black level.
+sensor = sensorSet(sensor,'zero level',64);
+sensor = sensorSet(sensor,'exp time',0.005);
+
+sensor = sensorCompute(sensor,oi);
+sensorWindow(sensor);
+
+% Compute from sensor to sRGB
+ip = ipCompute(ip,sensor);
+ipWindow(ip);
+
+testsRGB = ipGet(ip,'srgb');
 testLum = sum(test,3)/3;
 
 %%
-ieNewGraphWin;
-montage({ref,test});
-
-% To make life simpler, and because SSIM doesn't care, we scale the lum
-mx = max(max(lumTest(:)),max(lumRef(:)));
-lumTest = lumTest/mx;
-lumRef  = lumRef/mx;
 
 ieNewGraphWin;
-montage({lumTest,lumRef});
-
-%% 24 patches.  Poisson noise illustrated
+montage({refLum,testLum});
 
 ieNewGraphWin;
-plot(lumRef(1:10:end),lumTest(1:10:end),'k.');
-identityLine; grid on;
+montage({refsRGB,testsRGB});
 
-%% The three measures
+%% Read for metrics
 
-% None of this closely matches the Matlab calculation.
-% I should figure out why.  So many possible reasons.
-
-% SSIM constants when image is scaled between 0 and 1
-C1 = 0.01^2;
-C2 = 0.03^2;
-C3 = C2/2;
-
-ux = mean(lumRef(:));
-uy = mean(lumTest(:));
-
-sdx = std(lumRef(:));
-sdy = std(lumTest(:));
-
-tmp = cov(lumRef,lumTest);
-sxy = tmp(1,2);
-
-% Global formula
-((2*ux*uy + C1)*(2*sxy + C2)) / ((ux^2 + uy^2 + C1)*(sdx^2 + sdy^2 + C2))
-
-%%  Compute the luminance
-
-[val,ssimmap] = ssim(test,ref);
+[val,ssimmap] = ssim(testLum,refLum);
 
 % SSIM 1 is highest quality.  We want this to be an error map, so we
 % subtract from one.
@@ -89,27 +90,19 @@ colorbar; axis image; axis off
 ieNewGraphWin;
 mesh(1 - mean(ssimmap,3));
 
-%% Try S-CIELAB on these images?
+%% Now try S-CIELAB
 
 params = scParams;
-fov = 40;
+fov = 10;
 params.sampPerDeg = round(size(test,1)/fov);
 testXYZ = ieClip(srgb2xyz(test),0,[]);
-refXYZ  = ieClip(srgb2xyz(ref),0,[]);
+refXYZ  = ieClip(srgb2xyz(refsRGB),0,[]);
 
 dEimg = scielab(testXYZ,refXYZ,[.92 .98 .98],params);
 ieNewGraphWin; imagesc(dEimg);
 colorbar; axis image; axis off
 
-
-% ieNewGraphWin;
-% mesh(dEimg);
-% mean(dEimg(:))
-
-%%
 ieNewGraphWin;
-g = fspecial("gaussian",[27,27],9);
-imagesc(conv2(dEimg,g)); axis image
-title('SCIELAB Error')
+mesh(dEimg);
 
 %%
