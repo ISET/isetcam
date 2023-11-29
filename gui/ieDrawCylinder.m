a40d24da3b7d6a3223133edb39b7968da127bd03
Brian Wandell <1837145+wandell@users.noreply.github.com>
2023-11-28 16:40:16 -0800

    Update ieDrawCylinder.m

--- gui/ieDrawCylinder.m
+++ gui/ieDrawCylinder.m
@@ -1,8 +1,16 @@
-function drawCylinder(v, radius, height, resolution)
-% v: Direction vector
-% radius: Radius of the cylinder
-% height: Height of the cylinder
-% resolution: Number of sides for the cylinder (e.g., 20 for a smooth appearance)
+function hdl = ieDrawCylinder(v, varargin)
+% Draw a cylinder
+%
+% Inputs
+%   v: Direction vector of main axis
+%
+% Key/val pairs
+%   radius:     Radius of the cylinder
+%   height:     Height of the cylinder
+%   resolution: Number of sides for the cylinder (e.g., 20 for a smooth appearance)
+%
+% Outputs
+%   hdl - Figure handle
 %
 % Status:  It draws a cylinder with some height and radius.  But the
 % direction is not as expected.
@@ -18,9 +26,25 @@ direction = [1, 0, 0]; % Replace with your desired direction vector
 radius = 5.1;
 height = 20;
 resolution = 20;
-drawCylinder(direction, radius, height, resolution);
+hdl = ieDrawCylinder(direction);
 line([0,10*direction(1)],[0,10*direction(2)],[0,10*direction(3)]);
 %}
+
+%% Parse
+p = inputParser;
+p.addRequired('v',@isvector);
+p.addParameter('radius',5,@isnumeric);
+p.addParameter('height',20,@isnumeric)
+p.addParameter('resolution',20,@isnumeric)
+
+p.parse(v,varargin{:});
+
+radius = p.Results.radius;
+height = p.Results.height;
+resolution = p.Results.resolution;
+
+%%
+
 % Generate points for the base and top circles of the cylinder
 theta = linspace(0, 2*pi, resolution);
 x_base = radius * cos(theta);
@@ -46,7 +70,8 @@ y_side = [base_points(2, :); top_points(2, :)];
 z_side = [base_points(3, :); top_points(3, :)];
 
 % Plot the cylinder
+hdl = ieNewGraphWin;
 surf(x_side, y_side, z_side, 'FaceColor', 'b', 'EdgeColor', 'none');
-axis equal;
-view(3);
+axis equal; rotate3d; view(3);
+
 end
