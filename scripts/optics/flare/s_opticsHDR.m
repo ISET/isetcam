fd9bc4de0fc6d3091636753632953867a0cf7dc7
Zhenyi Liu <zhenyi27@stanford.edu>
2023-12-26 22:28:55 -0800

    Add assertion

--- scripts/optics/flare/s_opticsHDR.m
+++ scripts/optics/flare/s_opticsHDR.m
@@ -19,45 +19,104 @@
 ieInit;
 clear all; close all
 %%
-s_size = 1280;
-flengthM = 5e-3;
+s_size = 512;
+flengthM = 4e-3;
 fnumber = 4;
+
+
 pupilMM = (flengthM*1e3)/fnumber;
 
-scene = sceneCreate('point array',s_size,s_size);
-scene = sceneSet(scene,'fov',20);
+scene = sceneCreate('point array',s_size,s_size/4);
+scene = sceneSet(scene,'fov',10);
+d = create_shapes();
+wave = 400:10:700;
+illPhotons = Energy2Quanta(wave,ones(31,1))*1e3;
 
-oi = oiCreate('shift invariant');
+data = bsxfun(@times, d, reshape(illPhotons, [1 1 31]));
+
+scene = sceneSet(scene,'illuminantPhotons',illPhotons);
+
+% Allocate space for the (compressed) photons
+scene = sceneSet(scene,'photons',data);
+
+scene = sceneAdjustLuminance(scene,'peak',100000);
+
+oi = oiCreate('diffraction limited');
 
 oi = oiSet(oi,'optics focallength',flengthM);
-oi = oiSet(oi,'optics fnumber',flengthM*1e3/pupilMM);
 
-% oi needs information from scene to figure out the proper resolution.
-oi = oiCompute(oi, scene);
-oiWindow(oi);
+for fnumber = 2:4:10
+    oi = oiSet(oi,'optics fnumber',fnumber);
 
-oi = oiSet(oi, 'name','icam');
-oi = oiSet(oi,'displaymode','hdr');
-ip = piRadiance2RGB(oi,'etime',1/30);
-ipWindow(ip)
+    % oi needs information from scene to figure out the proper resolution.
+    oi = oiCompute(oi, scene);
+    oiWindow(oi);
+
+    oi = oiSet(oi, 'name','icam');
+    oi = oiSet(oi,'displaymode','hdr');
+    ip = piRadiance2RGB(oi,'etime',0.1);
+    ipWindow(ip)
+
+    %% Match wvf with OI
+    % nsides = 0; % circular aperture
+    % 
+    % optics = oiGet(oi,'optics');
+    % wvf    = optics2wvf(optics);
+    % 
+    % [aperture, params] = wvfAperture(wvf,'nsides',nsides,...
+    %     'dot mean',50, 'dot sd',20, 'dot opacity',0.5,'dot radius',100,...
+    %     'line mean',50, 'line sd', 20, 'line opacity',0.5,'linewidth',80);
+    aperture = [];
+    wvf = wvfComputeFromOI(oi, aperture);
+
+    oi_wvf = oiCompute(wvf,scene);
+    oi_wvf = oiSet(oi_wvf, 'name','icam');
+
+    oiWindow(oi_wvf);
+    oi_wvf = oiSet(oi_wvf,'displaymode','hdr');
+    ip_wvf = piRadiance2RGB(oi_wvf,'etime',0.1);
+    ipWindow(ip_wvf);
+    
+    assert((mean(ip_wvf.data.result(:)) - mean(ip.data.result(:)))< 1/4096); % smaller than 1 digit for 12 bit
+end
 
 
-%% Match wvf with OI
-nsides = 5; % circular aperture
-[aperture, params] = wvfAperture(wvf,'nsides',nsides,...
-    'dot mean',50, 'dot sd',20, 'dot opacity',0.5,'dot radius',100,...
-    'line mean',50, 'line sd', 20, 'line opacity',0.5,'linewidth',80);
+function binary_mask = create_shapes()
+    % Create a blank image
+    image_size = 1024;
+    binary_mask = zeros(image_size, image_size);
 
-wvf = wvfComputeFromOI(oi, aperture);
+    % Draw a circle
+    center = [rand(500)+300, 100+rand(500)]; % center of the circle
+    radius = 20;
+    [x, y] = meshgrid(1:image_size, 1:image_size);
+    binary_mask((x - center(1)).^2 + (y - center(2)).^2 <= radius^2) = 1;
 
-oi_wvf = oiCompute(wvf,scene);
-oi_wvf = oiSet(oi_wvf, 'name','icam');
+    % Draw a rectangle
+    top_left = [500, 500];
+    width = 100;
+    height = 100;
+    binary_mask(top_left(1):(top_left(1)+height), top_left(2):(top_left(2)+width)) = 1;
 
-oiWindow(oi_wvf);
-oi_wvf = oiSet(oi_wvf,'displaymode','hdr');
-ip_wvf = piRadiance2RGB(oi_wvf,'etime',1/30);
-ipWindow(ip_wvf);
+    % % Draw a triangle
+    % vertices = [200, 300; 250, 400; 150, 400];
+    % binary_mask = insertShape(binary_mask, 'FilledPolygon', vertices(:)', 'Color', 'white', 'Opacity', 1);
 
+    % Draw a hexagon
+    center_hex = [750+rand(100), 750+rand(100)];
+    size_hex = 50;
+    angle = 0:pi/3:2*pi;
+    hex_x = center_hex(1) + size_hex * cos(angle);
+    hex_y = center_hex(2) + size_hex * sin(angle);
+    hexagon = [hex_x; hex_y];
+    binary_mask = insertShape(binary_mask, 'FilledPolygon', hexagon(:)', 'Color', 'white', 'Opacity', 1);
+
+    % Convert to binary
+    binary_mask = im2bw(binary_mask);
+
+    % Display the image
+    % imshow(binary_mask);
+end
 %{
 % Check some numbers
 fprintf('COMPLETED: Wavefront Size: %d, takes %.2f seconds. \n',nPixels, toc(t));
