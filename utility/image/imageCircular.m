3707b71c39ef90cb63f3ff2697c66d3fbbfe8511
David Brainard <brainard@psych.upenn.edu>
2023-12-31 15:51:13 -0500

    This was using the vector imageSize as in 1:imageSize. Not right
    The effect was to ignore the second dimension of imageSize in the meshgrid,  Something was also werid about the way centerPoint was being computed.
    
    I think with the fix, it will behave the same for square image input, and do something considerably more sensible for rectangular input.

--- utility/image/imageCircular.m
+++ utility/image/imageCircular.m
@@ -5,7 +5,7 @@ function im = imageCircular(im)
 %   im = imageCircular(im)
 %
 % Brief description
-%   By default, the radius is equal to half the image size.  Some day, put
+%   By default, the radius is equal to half the minimum image size dimension.  Some day, put
 %   in a radius parameter
 %
 % Input
@@ -18,11 +18,23 @@ function im = imageCircular(im)
 %   wvfPupilAmplitude
 
 % The radius is related to the image size
+%
+% DHB: Thy=is was previously written with the vector imageSize passed to
+% meshgrid. Matlab 2024 starting throwing a warning that this doesn't make
+% sense. I changed to pass imageSize(1) and imageSize(2) as the args
+% to meshgrid, and simplified calculation of centerPoint on the assumption
+% that imageSize is in fact a vector. Also, I used 2 as the index for the
+% x arg to meshgrid, and 1 as the index for the y arg.  This was the other
+% way around for centerPoint previously, but I think that is wrong.
 imageSize   = size(im);
-centerPoint = [imageSize/2 + 1, imageSize/2+1];
+if (length(imageSize) ~= 2)
+    error('Need to think about what to do if someone passes a vector as an image');
+end
+  
+centerPoint = imageSize/2 + 1;
 radius = (min(imageSize) - 1)/2;
 
-[X,Y] = meshgrid((1:imageSize) - centerPoint(1),(1:imageSize) - centerPoint(2));
+[X,Y] = meshgrid((1:imageSize(2)) - centerPoint(2),(1:imageSize(1)) - centerPoint(1));
 imRadius = sqrt(X.^2 + Y.^2);
 % ieNewGraphWin; imagesc(imRadius); colormap(gray); colorbar; axis image
 
