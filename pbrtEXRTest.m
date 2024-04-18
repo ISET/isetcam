fb7cb3d4d21e9be75f9f6ee28b13e999e132edb3
Brian Wandell <wandell@stanford.edu>
2024-04-18 09:16:05 -0700

    Update pbrtEXRTest.m

--- pbrtEXRTest.m
+++ pbrtEXRTest.m
@@ -15,3 +15,21 @@ sceneWindow(scene);
 
 % This should work, but doesn't.
 % depthmap = piEXR2ISET(fname,'label','depth');
+
+%%
+camera = cameraCreate;
+camera = cameraSet(camera,'oi',oiCreate);
+camera = cameraSet(camera,'sensor',sensorCreate);
+
+sensor = sensorSetSizeToFOV(cameraGet(camera,'sensor'),30);
+camera = cameraSet(camera,'sensor',sensor);
+
+camera = cameraCompute(camera,scene);
+
+%%  The imx363 color in the ip for the saturated lights is weird.
+% it is ok for the standard sensor.  Something about the IP processing is
+% off.
+camera = cameraSet(camera,'sensor exp time',0.01);
+camera = cameraCompute(camera,'oi');
+
+cameraWindow(camera,'ip');
