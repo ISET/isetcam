c28d5bc712087df47ab238d09e775d6563c19083
Brian Wandell <wandell@stanford.edu>
2024-04-20 17:21:01 -0700

    Update ieSCP.m

--- utility/file/ieSCP.m
+++ utility/file/ieSCP.m
@@ -1,8 +1,12 @@
-function status = ieSCP(user,host,src,destinationPath)
-% Remote copy a file 
+function [command,status] = ieSCP(user,host,src,destinationPath,varargin)
+% Remote copy a file or directory.
+%
+% Synopsis
+%    [command,status] = ieSCP(user,host,src,destinationPath,varargin)
 %
 % Description
-%   We securely copy a file or a folder from a remote machine.
+%   Uses scp to securely copy a file or a folder from a remote machine.
+%   NOTE:  Folder copy is not implemented and tested.  
 %
 % Input
 %   user - user name
@@ -12,31 +16,75 @@ function status = ieSCP(user,host,src,destinationPath)
 %
 % Optional
 %   folder - Copy a folder, not a file (default false)
+%   quiet  - Suppress printout to command window
 %
 % See also
-%
+%   s_autoLightGroups (isetauto)
 
-%% Replace with username, hostname, source and destination paths
+% Example:
 %{
-username = 'your_username';
-hostname = 'linux_machine_name';
-sourcePath = '/remote/folder/filename.txt';
-destinationPath = '/local/folder/';
+  % Just the metadata file
+  user = 'wandell';
+  host = 'orange.stanford.edu';
+  src  = '/acorn/data/iset/isetauto/Ford/SceneMetadata/1114091636.mat';
+  destPath = pwd;
+  ieSCP(user,host,src,destPath,'quiet',false);
+%}
+%{
+  % The metadata and the rendered images
+  user = 'wandell';
+  host = 'orange.stanford.edu';
+  
+  % Prepare the local directory  
+  imageID = '1114091636';
+  destPath = fullfile(iaRootPath,'local',imageID);
+  if ~exist(destPath,'dir'), mkdir(destPath); end
+
+  % First the metadata
+  src  = fullfile('/acorn/data/iset/isetauto/Ford/SceneMetadata',[imageID,'.mat']);
+  ieSCP(user,host,src,destPath);
+  load(fullfile(destPath,[imageID,'.mat']),'sceneMeta');
+
+  % Now the four light group EXR files
+  lgt = {'headlights','streetlights','otherlights','skymap'};
+  for ll = 1:numel(lgt)
+     thisFile = sprintf('%s_%s.exr',imageID,lgt{ll});
+     srcFile  = fullfile(sceneMeta.datasetFolder,thisFile); 
+     destFile = fullfile(destPath,thisFile);
+     ieSCP(user,host,srcFile,destFile);
+  end
+
 %}
 
-%% Construct the SSH command for scp
+%% Input args
 
-if p.Results.folder
-    command = ['scp -r ' user '@' host ':' src ' ' destinationPath];
-else
-    command = ['scp ' user '@' host ':' src ' ' destinationPath];
-end
+varargin = ieParamFormat(varargin);
+
+p = inputParser;
+p.addRequired('user',@ischar);
+p.addRequired('host',@ischar);
+p.addRequired('src',@ischar);    % Remote file or remote folder
+p.addRequired('destinationPath',@ischar);
+
+p.addParameter('folder',false,@islogical);  % Download the whole folder
+p.addParameter('quiet',false,@islogical);
+
+p.parse(user,host,src,destinationPath,varargin{:});
+
+%% Construct the SSH command for scp
+flags = '';
+if p.Results.folder, flags = [flags,' -r']; end
+if p.Results.quiet,  flags = [flags,' -q']; end
+
+command = ['scp ',flags, ' ', user, '@', host, ':', src, ' ', destinationPath];
 
 % Execute the command using system
 status = system(command);
 
 if status == 0
-  disp('File copied successfully!');
+  if ~p.Results.quiet, disp('File copied successfully!'); end
 else
-  disp('Error during copy. Check connection and paths.');
+  disp('Error during remote secure copy. Check connection and paths.');
+end
+
 end
