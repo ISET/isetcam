336a1ec2e61a462a16b55686c2852ec1c60f5181
Brian Wandell <wandell@stanford.edu>
2024-05-09 21:01:39 -0700

    Making sensor2EXR have noisy rgb

--- sensor/sensor2EXR.m
+++ sensor/sensor2EXR.m
@@ -1,33 +1,45 @@
-function filename = sensor2EXR(sensor,filename,varargin)
+function [filename, rgb] = sensor2EXR(sensor,filename,varargin)
 % Save sensor volts (default) or digital values into an EXR file
 %
 % Synopsis
-%   filename = sensor2EXR(sensor,filename,varargin)
+%   [filename, rgb] = sensor2EXR(sensor,filename,varargin)
 %
 % Input
 %   sensor   - Sensor after sensorCompute
 %   filename - If empty, a temporary file name is created and returned
 %
 % Key/val
-%  datatype  - 'volts' or 'dv'  (default: 'volts')
+%  dataformat - 'noisy rgb' or 'mosaic' (default: 'noisy rgb')
+%  datatype   - 'volts' or 'dv'  (default: 'volts')
+%  comment    -  Comment characters
+%  noiselevel -  Scale for uniform [0 1] noise to fill in (default: 0.02)
+%
+% Output
+%  filename   - EXR output file
+%  rgb        - Sensor data in RGB format for display
+%
+% Description
 %
 % TODO:
-%   Improve the comments that are placed in the Attributes field.
-%   Perhaps EXR format should be one of several options.
-%   There is no great way to save comments in the info slot
+%   The 'noisy rgb' is used for the Restormer network.
+%   Improve default the comments that are placed in the Attributes field.
 %
 % See also
 %   sensorSaveImage
 
 % Example:
 %{
-scene = sceneCreate; 
+scene = sceneCreate('freqorient',512); scene = sceneSet(scene,'fov',10);
 oi = oiCreate('wvf'); oi = oiCompute(oi,scene);
-sensor = sensorCreate; sensor = sensorCompute(sensor,oi);
+sensor = sensorCreate; 
+sensor = sensorSet(sensor,'fov',sceneGet(scene,'fov'),oi);
+sensor = sensorCompute(sensor,oi);
 
-fname = sensor2EXR(sensor,'','data type','volts');
+% A lot of noise for visibility.  But the default is quite small.
+[fname,rgb] = sensor2EXR(sensor,'','data type','volts','data format','noisyrgb','noise level',0.5);
+ieNewGraphWin; imagesc(rgb); axis image;
 
-img = exrread(fname); 
+img  = exrread(fname); 
 info = exrinfo(fname); disp(info.AttributeInfo.Comments)
 ieNewGraphWin; imagesc(img); colormap(gray)
 delete(fname);
@@ -41,11 +53,16 @@ p = inputParser;
 p.addRequired('sensor',@(x)(isstruct(x) && isequal(x.type,'sensor')))
 p.addRequired('filename',@(x)(ischar(x) || isstring(x)));
 p.addParameter('datatype','volts',@ischar);
-p.addParameter('comment','',@isstruct);
+p.addParameter('dataformat','noisyrgb',@ischar);
+p.addParameter('comment','',@ischar);
+p.addParameter('noiselevel',0.02,@isscalar);
 
 p.parse(sensor,filename,varargin{:});
 
-datatype = p.Results.datatype;
+datatype   = p.Results.datatype;
+dataformat = p.Results.dataformat;
+comment    = p.Results.comment;
+noiselevel = p.Results.noiselevel;
 
 %%
 switch datatype
@@ -55,11 +72,45 @@ switch datatype
         data = sensorGet(sensor,'dv');
 end
 
+switch dataformat
+    case 'mosaic'
+        % Leave data in mosaic form
+    case 'noisyrgb'
+        % Convert to separate planes with zero in the empty positions. This
+        % behavior is used by Zhenyi for the RGBW demosaicking experiments.
+        rgb = plane2rgb(data,sensor,0);
+        % ieNewGraphWin; imagesc(rgb); axis image
+
+        % Replace the zero values with a small amount of noise.
+        nChannels = size(rgb,3);
+        for ii=1:nChannels
+            tmp = rgb(:,:,ii);
+            noise = noiselevel * rand(size(tmp,1),size(tmp,2));
+            noise = noise .* (tmp==0);
+            % ieNewGraphWin; imagesc(noise); axis image
+            tmp = tmp + noise;
+            rgb(:,:,ii) = tmp;
+        end
+        data = rgb;
+        % ieNewGraphWin; imagesc(data); axis image
+    otherwise
+        error('unknown format %s',dataformat);
+end
+
 if isempty(filename), filename = [fullfile(tempname),'.exr']; end
 
+% Not sure if the RGB ordering is right.
 exrwrite(data,filename);
 info = exrinfo(filename);
+% system(sprintf('open %s',filename));
 
+if isempty(comment)
+    str = sprintf('Name: %s, Noiseflag: %d',sensorGet(sensor,'name'),sensorGet(sensor,'noise flag'));
+else
+    str = comment;
+end
+
+<<<<<<< Updated upstream
 pSize = sensorGet(sensor,'pixel size','um');
 str = sprintf('Name: %s | Exposure %.2f ms | Pixel-Size: %.2f um | N-Channels: %d |  Noise-flag: %d',...
     sensorGet(sensor,'name'),...    
@@ -67,6 +118,8 @@ str = sprintf('Name: %s | Exposure %.2f ms | Pixel-Size: %.2f um | N-Channels: %
     pSize(1), ...
     sensorGet(sensor,'nfilters'), ...
     sensorGet(sensor,'noise flag'));
+=======
+>>>>>>> Stashed changes
 info.AttributeInfo.Comments = str;
 exrwrite(data, filename, 'Attributes', info.AttributeInfo);
 
