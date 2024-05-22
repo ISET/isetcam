0c2cec78e8008efc4ed8caa43c5fcb31447b4a1b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-05-21 18:30:46 -0700

    Create wvfApertureP.m

---
+++ opticalimage/wavefront/wvfApertureP.m
@@ -0,0 +1,26 @@
+function params = wvfApertureP
+% Default parameters for wavefront aperture
+%
+% We will define these over time and illustrate their impact
+%
+% See also
+%   wvfAperture
+
+% Example:
+%{
+wvf = wvfCreate;
+params = wvfApertureP;
+[aperture, params] = wvfAperture(wvf,params);
+%}
+
+params.nsides      = 5;
+params.dotmean     = 10;
+params.dotsd       = 5;
+params.dotopacity  = 0.5;
+params.dotradius   = 5;
+params.linemean    = 10;
+params.linesd      = 5;
+params.lineopacity = 0.5;
+params.linewidth   = 2;
+
+end
