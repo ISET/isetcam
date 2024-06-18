9527a62b09c9ef5f09f8447ff933dee3356bfa06
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-06-18 11:24:59 -0700

    Documentation update

--- tutorials/development/t_oiRTCompute.m
+++ tutorials/development/t_oiRTCompute.m
@@ -1,16 +1,11 @@
 %% Running a ray trace calculation
 %
-% The ray-trace optics model is one of several types used in ISET
-% (diffraction and shift-invariant are the others).
-%
 % The ray trace model includes field-height and wavelength dependent
 % point spread functions. These can either be created synthetically,
-% or they can be derived from optics software, such as Zemax.  We will
-% start producing them with isetlens, as well.
+% or they can be derived from optics software, such as Zemax.  
 %
-% Copyright ImagEval Consultants, LLC, 2011.
-%
-% See also:  s_opticsRTGridLines, t_oiCompute
+% See also:  
+%    s_opticsRTGridLines, t_oiCompute
 
 %%
 ieInit
@@ -19,27 +14,27 @@ ieInit
 
 % You can adjust these parameters to speed up the calculation or
 % explore the lens properties
-scene = sceneCreate('point array',512,64);   % Creates an array of points
-scene = sceneSet(scene,'fov',20);            % Make this smaller after MH agrees.
+sz = 256; spacing = 64;
+scene = sceneCreate('point array',sz,spacing);   % Creates an array of points
+scene = sceneSet(scene,'fov',10);                % Make this smaller after MH agrees.
 
 % To speed the computatons we use a small number of wavelength samples
 scene = sceneInterpolateW(scene,(500:100:600));
 
 % Add the scene to the ISET database and view it
-ieAddObject(scene); sceneWindow; truesize
+sceneWindow(scene); drawnow;
 
 %% Create an optical image (oi) that uses the default ray trace model
 
-% This model has a default lens we converted from Zemax to the array of
-% point spread functions
+% This model is a lens we converted from Zemax to the array of point
+% spread functions.  There are various functions that illustrate how
+% to take Zemax data and convert them to the ISETCam ray trace
+% format.
 oi = oiCreate('ray trace');
 
-% If you would like to see the point spread functions, you can use this
-% code.
-%{
-   wave = 550; fhmm = 0.5;
-   rtPlot(oi,'psf',wave,fhmm);
-%}
+% Here is an example of one of the PSF
+wave = 550; fhmm = 0.5;
+rtPlot(oi,'psf',wave,fhmm);
 
 % Confirm that the optics model type is set to ray trace
 % In general, optics parameters can be read out with this syntax using an
@@ -49,12 +44,11 @@ fprintf('Ray trace lens:     %s\n',oiGet(oi,'optics rt name'))  % Name of the le
 
 %% The oiCompute will call opticsRayTrace to do the computation
 
-oi = oiCompute(oi,scene);
+% We do use 'crop' because there is no point in having the black
+% border around this scene.
+oi = oiCompute(oi,scene,'crop',true);
 
-% Make it easy to see the points
-sceneWindow; truesize
-
-% Here is the blurred result
-ieAddObject(oi); oiWindow; truesize
+% Here is the rendered blurred result
+oiWindow(oi);
 
 %%
