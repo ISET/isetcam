4ba46f6b0ea167992ae24d2583aaf906369c9243
Brian Wandell <wandell@stanford.edu>
2024-06-22 23:00:15 -0700

    vSNR improved for wiki page

--- imgproc/vcimageVSNR.m
+++ imgproc/vcimageVSNR.m
@@ -1,36 +1,68 @@
-function [vSNR,rect] = vcimageVSNR(vci,dpi,dist,rect)
+function [vSNR,rect] = vcimageVSNR(ip,dpi,dist,rect)
 % Calculate visual SNR in a uniform region of the vci
 %
-%   [vSNR,rect] = vcimageVSNR(vci,[dpi],[dist],[rect])
+% Should be renamed ipVSNR
 %
-% The visual SNR is the inverse of the standard deviation of the S-CIELAB
-% values in a uniform portion of an image.
+% Synopsis
+%   [vSNR,rect] = vcimageVSNR(ip,[dpi],[dist],[rect])
 %
-% Example
-%    vSNR = vcimageVSNR
+% Brief
+%   The visual SNR is the inverse of the standard deviation of the S-CIELAB
+%   values in a uniform portion of an image.
 %
-%    vci = vcGetObject('vci');
-%    dpi = 100;  % Relatively high sample
-%    dist = 0.20; % Seen at 18 inches
-%    vSNR = vcimageVSNR(vci,dpi,dist);
+% Input
+%  ip   - Image process with a computed image
+%  dist - Viewing distance of the display
+%  rect - Rectangular region of interest
 %
-%    vSNR = vcimageVSNR;
+% Output
+%   vSNR - Inverse of the st dev of the LAB values in the rect
+%   rect - Rectangle in the ip display image
 %
-% Copyright ImagEval Consultants, LLC, 2009
+% Description
+%  The typical PSNR is computed without reference to the eye or a display.
+%  The vSNR computed here puts the image data into S-CIELAB space,
+%  accounting for color and viewing distance.  The returned value, which is
+%  expected to be calculated over a uniform region, is the inverse of the
+%  standard deviation of the SCIELAB values in that region.
+%
+%  ieExamplesPrint('vcimageVSNR');
 %
 % See also
 %   xyz2vSNR
 
+% Example:
+%{
+ scene = sceneCreate('uniform ee',512); 
+ oi = oiCompute(oiCreate,scene);
+ sensor = sensorCreate; 
+ sensor = sensorSet(sensor,'pixel size same fill factor',[1.4 1.4]*1e-6);
+ sensor = sensorSet(sensor,'fov',5,oi);
+ sensor = sensorCompute(sensor,oi);
+ ip = ipCompute(ipCreate,sensor);
+
+ % Close, you can see the variation
+ dpi = ipGet(ip,'display dpi');  % Relatively high sample
+ rect = [18    13   215   174];
+ dist = 0.20; % Seen at 18 inches
+ [vSNR rect ] = vcimageVSNR(ip,dpi,dist,rect);
+ assert(abs(vSNR - 1.65) < 0.1)
+
+ % Further away the SNR is much better
+ dist = 2;  % Three meters
+ vSNR = vcimageVSNR(ip,dpi,dist,rect);
+ assert(abs(vSNR - 15) < 0.1)
+%}
 %%
-if ieNotDefined('vci'), vci = vcGetObject('vci'); end
+if ieNotDefined('vci'), ip = vcGetObject('vci'); end
 
 % Dots per inch on the display and subject's viewing distance
-if ieNotDefined('dpi'),  dpi  = ipGet(vci,'display DPI');   end
-if ieNotDefined('dist'), dist = ipGet(vci,'display Viewing Distance'); end
+if ieNotDefined('dpi'),  dpi  = ipGet(ip,'display DPI');   end
+if ieNotDefined('dist'), dist = ipGet(ip,'display Viewing Distance'); end
 
 % Select the ROI from the ipWindow_App
 if ieNotDefined('rect')
-    [roiLocs,roi] = ieROISelect(vci);
+    [roiLocs,roi] = ieROISelect(ip);
     rect = round(roi.Position);
 else
     roiLocs = ieRect2Locs(rect);
@@ -38,22 +70,14 @@ end
 
 % a = get(ipWindow,'CurrentAxes'); hold(a,'on'); ieDrawRect(a,rect)
 % Convert the data to an XYZ image
-roiXYZ = vcGetROIData(vci,roiLocs,'roixyz');          % Nx3
+roiXYZ = vcGetROIData(ip,roiLocs,'roixyz');          % Nx3
 
 % Reshape into an image
 c = rect(3)+1;r = rect(4)+1;
 roiXYZ = XW2RGBFormat(roiXYZ,r,c);
 
-%{
- figure(1);
- Y = roiXYZ(:,:,2); mesh(Y);
- colormap(jet(255)); mean(Y(:))
- srgb = xyz2srgb(roiXYZ); imagesc(srgb/255);
-%}
-
-
 % Set the SCIELAB parameters
-whitePtXYZ = ipGet(vci,'display White XYZ');
+whitePtXYZ = ipGet(ip,'display White XYZ');
 p = scParams(dpi,dist);
 
 % The filter shouldn't exceed the size of the image.
