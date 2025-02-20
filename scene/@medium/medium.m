e9875bb658a064f677ebe79d5bc7adbfb627fbb7
Brian Wandell <wandell@stanford.edu>
2025-02-20 12:03:05 -0800

    Update medium.m

--- scene/@medium/medium.m
+++ scene/@medium/medium.m
@@ -6,8 +6,11 @@ classdef medium < hiddenHandle
 %
 % Subclasses of @medium inherit spectral properties and methods from
 % @medium.  Using this superclass scheme, enforces consistency amongst all
-% subclasses in terms of how they handle spectral properties.  We might
-% make the blood types subclasses.  Not sure.
+% subclasses in terms of how they handle spectral properties.  So far,
+% though, we have no subclasses.
+%
+% A file can contain multiple absorbances.  Better have good comments for
+% those!
 %
 % Description:
 %    Our understanding of the terminology for describing medium
@@ -17,11 +20,13 @@ classdef medium < hiddenHandle
 %    columns.  This applies to all of the parameters below.
 %
 %    obj.absorbance  - the absorbance spectra of the medium, normalized to
-%    a peak value of 1. The normalization to peak of 1 is just a
-%    convention, the quantity that matters is the product
-%    obj.opticalDensity*ojb.absorbance. Some might call that product the
-%    absorbance.  But we prefer to keep the density and normalized
-%    absorbance separate.
+%    a peak value of 1. This describes the absorbed light for a thin sheet
+%    of the medium. 
+% 
+%    The normalization to peak of 1 is a convention that lets us explicitly
+%    specify the density: obj.opticalDensity*ojb.absorbance. Some
+%    scientists might call the product the absorbance, but we prefer to
+%    keep the density and normalized absorbance separate.
 %
 %    (Re ISETBio: This quantity is called obj.unitDensity in the Lens
 %    and Macular objects, and in the Lens object it is not normalized. 
@@ -72,44 +77,35 @@ classdef medium < hiddenHandle
 % Example:
 %{
    deoxyblood = medium('deoxyHemoglobin.mat');
+   deoxyblood.comment
 %}
 %{
   oxyblood = medium('oxyHemoglobin.mat','wave',400:1:700);
+  oxyblood.comment
+%}
+%{
+  % Hmm.  This has four terms but only three are in the comment
+  skin = medium('SkinComponentAbsorbances.mat','wave',400:1:700);
+  ieNewGraphWin; plot(skin.wave,skin.transmittance(:,1));
 %}
 
 % Public properties 
 properties 
     % opticalDensity - photopigment optical densities for different cone types
     opticalDensity;
-
-    % peakEfficiency - peak absorptance efficiency
-    % peakEfficiency;
-    
 end
 
 properties (Dependent)
 
-    % absorbance - spectral absorbance of the cones
+    % spectral absorbance of the medium
     absorbance;
     
-    % absorptance - cone absorptance without ocular media
+    % absorptance - absorptance without ocular media
     absorptance;
 
-    % % quantaFundamentals - normalized cone absorptance. Because of the
-    % % normalization, not useful for actual calculation, only for examining
-    % % the relative shape.
-    % quantaFundamentals;
-    % 
-    % % energyFundamentals - normalized cone absorptance converrted
-    % % to energy units from the normalized obj.quantaFundamentals.
-    % % Useful only for shape, because of the normalization.
-    % energyFundamentals;
-    % 
-    % % quantalEfficiency - probability of isomerization in quantal units.
-    % % Gives the probability that an incident photon isomerizes
-    % % photopigment. These are actually useful.  Does not take into account
-    % % effect of inert pigments (lens, macular pigment).
-    % quantalEfficiency;
+    % transmittance is 1 - absorptance
+    transmittance;
+    
 end
 
 properties (SetAccess = protected)
@@ -125,6 +121,7 @@ end
 properties (SetObservable, AbortSet)
     % wave - wavelength samples
     wave;
+    comment;
 end
 
 properties (SetAccess = public)
@@ -160,157 +157,36 @@ methods
         p.addParameter('wave',400:10:700,@isvector);
         p.parse(filename,varargin{:});
         
+        % Normalize the absorbance to a peak of 1
         obj.filename = which(filename);
-        [obj.absorbance_,obj.wave_] = ieReadSpectra(filename);
+        [obj.absorbance_,obj.wave_, obj.comment] = ieReadSpectra(filename);
+        obj.absorbance_ = obj.absorbance_/max(obj.absorbance_(:));
 
-        % set object properties
+        % set wavelength and specified optical density
         obj.wave = p.Results.wave(:); 
         obj.opticalDensity = p.Results.opticaldensity(:);
 
     end % Constructor
     
-    % Getter methods for dependent variables
-    function val = get.absorbance(obj) % interpolate for absorbance
-        % Retrieve photo pigment object's absorbance value
-        %
-        % Syntax:
-        %   obj = get.absorbance(obj)
-        %
-        % Description:
-        %    Retrieve the absorbance from the photoPigment object obj
-        %
-        % Inputs:
-        %    obj - The photoPigment object
-        %
-        % Outputs:
-        %    val - The absorbance value for obj
-        %
-        % Optional key/value pairs:
-        %    None.
-        
-        % DHB: Careful handling of long wavelengths beyond those in our table of
-        % photopigment absorbance. We linearly extrapolate the values out,
-        % but do so not just on the last 1 nm base but on a larger spacing
-        % to stablilize the extrapolation.  Perhaps too clever by half but
-        % we need estimates for wavelengths greater then the 830 in our
-        % typical table for AO modeling.  We don't need these to be
-        % perfect, but the extrapolation based on the last two points was
-        % not good. I wrote this not to change the behavior if no requested
-        % wavelengths exceed those in the table.
-        maxWlInTable = max(obj.wave_(:));
-        index2 = find(obj.wave > maxWlInTable);
-        if (isempty(index2))
-            val = interp1(obj.wave_, obj.absorbance_, obj.wave, ...
-                'linear', 'extrap');
-        else
-            extrapolationBaseNm = 30;
-            index1 = find(obj.wave <= maxWlInTable);
-            temp1 = interp1(obj.wave_, obj.absorbance_, obj.wave(index1), ...
-                'linear', 'extrap');
-            index3 = find(obj.wave_ < maxWlInTable - extrapolationBaseNm);
-            if (isempty(index3))
-                error('Do not have enough data in table for requested long wavelength extrapolation base');
-            else
-                index4 = find(obj.wave_ == maxWlInTable);
-                if (length(index4) ~= 1)
-                    error('We do not understand something really basic about Matlab');
-                end
-                temp2 = 10.^interp1(obj.wave_([index3(end) index4(1)]), log10(obj.absorbance_([index3(end) index4(1)],:)), obj.wave(index2), ...
-                    'linear', 'extrap');
-                val = [temp1 ; temp2];
-            end
-        end
-        val = ieClip(val, 0, 1);
+    
+    % Interpolate from the original data
+    % So far, I am not saving the interpolated data.  Maybe I should and
+    % then update it when 'wave' changes.
+    function val = get.absorbance(obj)
+        val = interp1(obj.wave_,obj.absorbance_,obj.wave,'pchip');
     end
 
+    % absorptance and transmittance are complements
+    function val = get.transmittance(obj)
+        val = 1 - obj.absorptance;
+    end
+
+
     function val = get.absorptance(obj) % compute absorptance
-        % Retrieve photo pigment object's absorptance value
-        %
-        % Syntax:
-        %   obj = get.absorptance(obj)
-        %
-        % Description:
-        %    Retrieve the absorptance from the photoPigment object obj
-        %
-        % Inputs:
-        %    obj - The photoPigment object
-        %
-        % Outputs:
-        %    val - The absorptance value for obj
-        %
-        % Optional key/value pairs:
-        %    None.
-        %
         val = 1 - 10 .^ (-obj.absorbance * diag(obj.opticalDensity));
     end
 
-    % function val = get.quantalEfficiency(obj) % compute absorptance
-    %     % Retrieve photo pigment object's isomerization efficiency.
-    %     %
-    %     % Syntax:
-    %     %   obj = get.quantalEfficiency(obj)
-    %     %
-    %     % Description:
-    %     %    Retrieve the quantal efficiency from the photoPigment object obj
-    %     %
-    %     % Inputs:
-    %     %    obj - The photoPigment object
-    %     %
-    %     % Outputs:
-    %     %    val - The LMS quantal efficiencies for obj
-    %     %
-    %     % Optional key/value pairs:
-    %     %    None.
-    %     %
-    %     val = obj.absorptance*diag(obj.peakEfficiency);
-    % end
-    % 
-    % function val = get.quantaFundamentals(obj)
-    %     % compute and return quanta fundamentals
-    %     %
-    %     % Syntax:
-    %     %   obj = get.quantaFundamentals(obj)
-    %     %
-    %     % Description:
-    %     %    Compute and return the quanta fundamentals for the photo
-    %     %    pigment object obj.
-    %     %
-    %     % Inputs:
-    %     %    obj - The photoPigment object
-    %     %
-    %     % Outputs:
-    %     %    val - The quanta fundamentals for obj
-    %     %
-    %     % Optional key/value pairs:
-    %     %    None.
-    %     %
-    %     val = bsxfun(@rdivide, obj.absorptance, max(obj.absorptance));
-    % end
-    % 
-    % function val = get.energyFundamentals(obj)
-    %     % Retrieve photo pigment object's energy fundamentals
-    %     %
-    %     % Syntax:
-    %     %   obj = get.energyFundamentals(obj)
-    %     %
-    %     % Description:
-    %     %    Retrieve the energy fundamentals from the photoPigment object
-    %     %
-    %     % Inputs:
-    %     %    obj - The photoPigment object
-    %     %
-    %     % Outputs:
-    %     %    val - The energy fundamentls for obj
-    %     %
-    %     % Optional key/value pairs:
-    %     %    None.
-    %     %
-    %     h = vcConstants('planck');
-    %     c = vcConstants('speed of light');
-    %     val = 1e-9 * bsxfun(@times, obj.quantaFundamentals / h / c, ...
-    %         obj.wave);
-    % end
-    % 
+    
 end % Public methods
 
 
