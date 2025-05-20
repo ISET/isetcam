44af8778cf6df27990d9e7140472f03353959b6b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2025-05-20 16:57:24 -0700

    Update iePlot.m

--- utility/plots/iePlot.m
+++ utility/plots/iePlot.m
@@ -1,10 +1,10 @@
 function p = iePlot(varargin)
 % Working on a method to eliminate all those ieNewGraphWin calls
 %
-%
+% I created ieFigure.  This method takes the plot() arguments and
+% passes them along after opening up a figure.
 
-ieNewGraphWin;
-
+ieFigure;
 p = plot(varargin{:});
 
 end
